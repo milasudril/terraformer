f2404aef3930fe1cf50f2790cada839d300c3146
Torbjörn Rathsman <milasudril@gmail.com>
2021-07-17 21:30:04 +0200

    Introduce randomness to make_ridge

--- main.cpp
+++ main.cpp
@@ -6,19 +6,26 @@
 
 #include <pcg_random.hpp>
 
+#include <numbers>
+#include <random>
 
-PolygonChain<float> make_ridge(Extents<Image::IndexType> ex)
+PolygonChain<float> make_ridge(Extents<Image::IndexType> ex, pcg32& rng)
 {
 	auto const ex_new = Extents{static_cast<float>(ex.width()), 0.5f*ex.depth()};
 	auto const arc = make_arc(ex_new);
 	auto const s = length(arc);
-	auto const dθ = static_cast<float>(arc.angle/s);
+	auto const dθ_max = 256.0f*static_cast<float>(arc.angle/s);
+//	printf("%.8g\n", dθ_max/std::numbers::pi_v<float>);
+	auto angle_dist = std::uniform_real_distribution{-dθ_max, dθ_max};
 	Point const pos_init{0.0f, ex_new.depth(), 1.0f};
-	auto pos = pos_init + Vector{std::cos(dθ), -std::sin(dθ), 0.0f};;
+	auto θ = 0.0f;
+	θ += angle_dist(rng);
+	auto pos = pos_init + Vector{std::cos(θ), -std::sin(θ), 0.0f};
 	PolygonChain ret{pos_init, pos};
 	for(int k = 1; k < static_cast<int>(s); ++k)
 	{
-		pos += Vector{std::cos(k*dθ), -std::sin(k*dθ), 0.0f};;
+		pos += Vector{std::cos(θ), -std::sin(θ), 0.0f};;
+		θ += angle_dist(rng);
 		ret.append(pos);
 	}
 
@@ -36,8 +43,10 @@ void debug(GrayscaleImage const& img)
 
 int main()
 {
+	pcg32 rng;
+
 	GrayscaleImage img{1024, 512};
-	auto ridge = make_ridge(img.extents());
+	auto ridge = make_ridge(img.extents(), rng);
 
 	std::ranges::for_each(ridge.vertices(), [&img](auto pos) {
 		printf("%.8g %.8g\n", pos.x(), pos.y());
