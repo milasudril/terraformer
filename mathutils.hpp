6b4b7d06a558b0366f5f0cb2fd17daec5f890762
Torbjörn Rathsman <milasudril@gmail.com>
2021-07-17 15:37:44 +0200

    Move definition of vec_t

--- mathutils.hpp
+++ mathutils.hpp
@@ -2,36 +2,12 @@
 #define TERRAFORMER_MATHUTILS_HPP
 
 #include "./adj_algo.hpp"
+#include "./vec_t.hpp"
 
 #include <cmath>
 #include <vector>
 #include <span>
 #include <type_traits>
-
-template<class T>
-requires std::is_arithmetic_v<T> && (!std::is_const_v<T>)
-using vec4_t [[gnu::vector_size(4*sizeof(T))]] = T;
-
-template<class U>
-struct HasVectorSize:std::false_type{};
-
-template<>
-struct HasVectorSize<vec4_t<float>>:std::true_type{};
-
-template<>
-struct HasVectorSize<vec4_t<int>>:std::true_type{};
-
-template<>
-struct HasVectorSize<vec4_t<double>>:std::true_type{};
-
-template<>
-struct HasVectorSize<vec4_t<long long>>:std::true_type{};
-
-template<class T, class U>
-vec4_t<T> vector_cast(U v) requires HasVectorSize<U>::value
-{
-	return vec4_t<T>{static_cast<T>(v[0]), static_cast<T>(v[1]), static_cast<T>(v[2]), static_cast<T>(v[3])};
-}
 
 template<class T>
 constexpr T unity()
--- mathutils.test.cpp
+++ mathutils.test.cpp
@@ -4,16 +4,6 @@
 
 #include "testfwk/testfwk.hpp"
 
-TESTCASE(VectorCast)
-{
-	vec4_t<float> const v{1.0f, 1.75f, 1.25f, -1.5f};
-	auto const v1 = vector_cast<int>(v);
-	static_assert(std::is_same_v<decltype(v1), vec4_t<int> const>);
-	vec4_t<int> const expected{1, 1, 1, -1};
-	for(int k = 0; k < 4; ++k)
-	{ EXPECT_EQ(v1[k], expected[k]); }
-}
-
 TESTCASE(Unity)
 {
 	constexpr auto val = unity<int>();
