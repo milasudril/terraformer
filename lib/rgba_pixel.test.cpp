4d4431ccc0bd17c04c8733be413a80d350cd15e0
Torbjörn Rathsman <milasudril@gmail.com>
2023-01-01 10:18:48 +0100

    Clean repo

--- lib/rgba_pixel.test.cpp
+++
@@ -1,9 +0,0 @@
-//@	{"target":{"name":"pixel.test"}}
-
-#include "./rgba_pixel.hpp"
-
-#include "testfwk/testfwk.hpp"
-
-#include <type_traits>
-
-static_assert(std::is_trivial_v<RgbaPixel>);
--- lib/span_2d.test.cpp
+++
@@ -1,5 +0,0 @@
-//@	{"target":{"name":"span_2d.test"}}
-
-#include "./span_2d.hpp"
-
-#include "testfwk/testfwk.hpp"
