7d613a6ebbe8674ee7b23fe003732e14acfde2b1
Torbjörn Rathsman <milasudril@gmail.com>
2023-01-01 10:38:11 +0100

    Add type aliases for geomtric stuff

---
+++ lib/spaces.hpp
@@ -0,0 +1,17 @@
+#ifndef TERRAFORMER_LIB_SPACES_HPP
+#define TERRAFORMER_LIB_SPACES_HPP
+
+#include <geosimd/euclidian_space.hpp>
+
+namespace terraformer
+{
+	struct geom_space : geosimd::euclidian_space<float, 3>{};
+
+	using location = geom_space::location;
+	using displacement = geom_space::displacement;
+	using direction = geom_space::direction;
+
+	inline constexpr auto origin = geosimd::origin<geom_space>();
+};
+
+#endif
---
+++ lib/spaces.test.cpp
@@ -0,0 +1,18 @@
+//@	{"target":{"name":"spaces.test.cpp"}}
+
+#include "./spaces.hpp"
+
+#include "testfwk/testfwk.hpp"
+
+TESTCASE(terraformer_lib_spaces_type_properties)
+{
+	EXPECT_EQ(geosimd::point<terraformer::location>, true);
+	EXPECT_EQ(geosimd::vector<terraformer::location>, false);
+	EXPECT_EQ(terraformer::location::size(), 3);
+
+	EXPECT_EQ(geosimd::vector<terraformer::displacement>, true);
+	EXPECT_EQ(terraformer::displacement::size(), 3);
+
+	EXPECT_EQ(geosimd::vector<terraformer::direction>, false);
+	EXPECT_EQ(terraformer::direction::size(), 3);
+}
