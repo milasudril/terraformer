b6f7139e4e11a5605baff8b4479e41871b416a3c
Torbjörn Rathsman <milasudril@gmail.com>
2023-01-07 18:56:25 +0100

    Add more testcases

--- lib/damped_motion_integrator.test.cpp
+++ lib/damped_motion_integrator.test.cpp
@@ -17,6 +17,34 @@ TESTCASE(terraformer_damped_motion_integrator_step_response_at_min_inertia)
 
 	state = integrator(state, v);
 	EXPECT_EQ(state.v, v);
+	EXPECT_EQ(state.r, (terraformer::location{0.5f, 0.0f, 0.0f}));
 	state = integrator(state, v);
 	EXPECT_EQ(state.v, v);
+	EXPECT_EQ(state.r, (terraformer::location{1.5f, 0.0f, 0.0f}));
+	state = integrator(state, v);
+	EXPECT_EQ(state.v, v);
+	EXPECT_EQ(state.r, (terraformer::location{2.5f, 0.0f, 0.0f}));
+}
+
+TESTCASE(terraformer_damped_motion_integrator_step_response_inertia_1dot5)
+{
+	constexpr terraformer::displacement v{1.0f, 0.0f, 0.0f};
+	terraformer::particle_state state{};
+	terraformer::damped_motion_integrator integrator{v,
+		terraformer::damped_motion_integrator::params{
+			.inertia = 1.5f,
+			.curve_scaling_factor = 1.0f
+		}
+	};
+
+	auto exponent = 2.0f;
+	for(size_t k = 0; k != 8; ++k)
+	{
+		state = integrator(state, v);
+		auto [vx, vy, vz] = state.v;
+		EXPECT_EQ(vx, (1.0f - 1.0f/exponent));
+		EXPECT_EQ(vy, 0.0f);
+		EXPECT_EQ(vz, 0.0f);
+		exponent *= 2.0f;
+	}
 }
