6b0eb58f794c155dd232ab47b96203ab37f75735
Torbjörn Rathsman <milasudril@gmail.com>
2023-01-15 14:52:24 +0100

    Add raw utility to convert from heightmap (exr) to mesh (obj)

---
+++ bin/heightmap2mesh.cpp
@@ -0,0 +1,32 @@
+//@	{"target":{"name":"heightmap2mesh.o"}}
+
+#include "lib/pixel_store/image_io.hpp"
+#include "lib/functions/heightmap_to_mesh.hpp"
+#include "lib/mesh_store/mesh_output.hpp"
+
+int main(int argc, char** argv)
+{
+	if(argc < 1 + 1 + 3 + 1)
+	{
+		puts("Usage heightmap2mesh input sx sy sz output");
+		return 1;
+	}
+
+	auto const src = argv[1];
+	auto const s_x = static_cast<float>(atof(argv[2]));
+	auto const s_y = static_cast<float>(atof(argv[3]));
+	auto const s_z = static_cast<float>(atof(argv[4]));
+	auto const dest = argv[5];
+
+	auto const mesh = create(std::type_identity<terraformer::mesh>{},
+		terraformer::heightmap{
+			load(std::type_identity<terraformer::grayscale_image>{}, src),
+			s_x,
+			s_y,
+			s_z,
+		});
+
+	store(mesh, dest);
+
+	return 0;
+}
