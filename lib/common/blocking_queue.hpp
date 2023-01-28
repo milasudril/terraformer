84d7220c2020ad97c04b0e89203621204b795191
Torbjörn Rathsman <milasudril@gmail.com>
2023-01-28 06:46:51 +0100

    Fix blocking queue

--- lib/common/blocking_queue.hpp
+++ lib/common/blocking_queue.hpp
@@ -1,7 +1,6 @@
 #ifndef TERRAFORMER_DIFFUSER_BLOCKING_QUEUE_HPP
 #define TERRAFORMER_DIFFUSER_BLOCKING_QUEUE_HPP
 
-#include <shared_mutex>
 #include <mutex>
 #include <queue>
 
@@ -13,13 +12,13 @@ namespace terraformer
 	public:
 		void push(T&& cb)
 		{
-			write_lock lock{m_mutex};
+			std::lock_guard lock{m_mutex};
 			m_queue.push(std::move(cb));
 		}
 
 		std::optional<T> try_pop()
 		{
-			write_lock lock{m_mutex};
+			std::lock_guard lock1{m_mutex};
 			if(m_queue.empty())
 			{ return std::nullopt; }
 
@@ -29,9 +28,7 @@ namespace terraformer
 		}
 
 	private:
-		using read_lock = std::unique_lock<std::shared_mutex>;
-		using write_lock = std::shared_lock<std::shared_mutex>;
-		std::shared_mutex m_mutex;
+		std::mutex m_mutex;
 		std::queue<T> m_queue;
 	};
 }
