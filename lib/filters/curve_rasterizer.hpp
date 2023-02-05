e5e5ab69a51eb1b54cce37ae50a24c4c1f233282
Torbjörn Rathsman <milasudril@gmail.com>
2023-02-05 16:40:10 +0100

    Use new struct as parameter to paint algorithm

--- lib/filters/curve_rasterizer.hpp
+++ lib/filters/curve_rasterizer.hpp
@@ -56,29 +56,28 @@ namespace terraformer
 		blend_function<PixelType> BlendFunction = lerp<PixelType>>
 	struct paint_params
 	{
-		float x_0;
-		float y_0;
+		float x;
+		float y;
 		PixelType value;
 		float brush_diameter;
-		Brush brush;
-		BlendFunction blend_function;
+		Brush brush{};
+		BlendFunction blend_function{};
 	};
 
-	template<class PixelType, brush Brush = solid_circle>
-	void draw(span_2d<PixelType> target_surface,
-		float x,
-		float y,
-		PixelType value,
-		float d,
-		Brush&& brush = Brush{})
+	template<class PixelType,
+		brush Brush,
+		blend_function<PixelType> BlendFunction>
+	void paint(span_2d<PixelType> target_surface, paint_params<PixelType, Brush, BlendFunction> const& params)
 	{
 		auto const h = target_surface.width();
 		auto const w = target_surface.height();
 
+		auto const value = params.value;
+		auto const d = params.brush_diameter;
 		auto const thickness = 0.5f*d;
 		auto const brush_size = static_cast<uint32_t>(d);
-		auto const k_min = static_cast<uint32_t>(y - thickness + 0.5f);
-		auto const l_min = static_cast<uint32_t>(x - thickness + 0.5f);
+		auto const k_min = static_cast<int32_t>(params.y - thickness + 0.5f);
+		auto const l_min = static_cast<int32_t>(params.x - thickness + 0.5f);
 
 		for(auto k = 0u; k != brush_size; ++k)
 		{
@@ -87,9 +86,11 @@ namespace terraformer
 				auto const xi = std::lerp(-1.0f, 1.0f, (static_cast<float>(l) + 0.5f)/d);
 				auto const eta = std::lerp(-1.0f, 1.0f, (static_cast<float>(k) + 0.5f)/d);
 
-				auto const strength = brush(xi, eta);
+				auto const strength = params.brush(xi, eta);
 				auto const src_val = target_surface((l + l_min + w)%w, (h - 1) - (k + k_min + h)%h);
-				target_surface((l + l_min + w)%w, (h - 1) - (k + k_min + h)%h ) = strength*value + (1.0f - strength)*src_val;
+				static_assert(std::is_same_v<decltype(l + l_min), uint32_t>);
+				target_surface((l + l_min + w)%w, (h - 1) - (k + k_min + h)%h ) =
+					params.blend_function(src_val, value, strength);
 			}
 		}
 	}
@@ -118,7 +119,13 @@ namespace terraformer
 					+ seg.p1[1];
 				auto const z = b*static_cast<float>(l - static_cast<int32_t>(seg.p1[0])) + seg.p1[2];
 				auto const d = mod(x, y);
-				draw(target_surface, x, y, z*value, d, brush);
+				paint(target_surface, paint_params{
+					.x = x,
+		  			.y = y,
+		  			.value = z*value,
+		  			.brush_diameter = d,
+					.brush = brush
+				});
 			}
 		}
 		else
@@ -135,7 +142,13 @@ namespace terraformer
 					+ seg.p1[0];
 				auto const z = b*static_cast<float>(k - static_cast<int32_t>(seg.p1[1])) + seg.p1[2];
 				auto const d = mod(x, y);
-				draw(target_surface, x, y, z*value, d, brush);
+				paint(target_surface, paint_params{
+					.x = x,
+		  			.y = y,
+		  			.value = z*value,
+		  			.brush_diameter = d,
+					.brush = brush,
+				});
 			}
 		}
 	}
@@ -160,22 +173,6 @@ namespace terraformer
 			prev = current;
 		}
 	}
-
-	template<class PixelType,
-		brush_size_modulator BrushSizeModulator = default_brush_size_modulator,
-		brush Brush = solid_circle>
-	void draw_as_dots(span_2d<PixelType> target_surface,
-		std::span<location const> curve,
-		PixelType value,
-		BrushSizeModulator&& mod = BrushSizeModulator{},
-		Brush&& brush = Brush{})
-	{
-		for(auto point: curve)
-		{
-			draw(target_surface, point, value, mod, brush);
-		}
-	}
-
 }
 
 #endif
--- lib/filters/curve_rasterizer.test.cpp
+++ lib/filters/curve_rasterizer.test.cpp
@@ -11,8 +11,13 @@ TESTCASE(terraformer_draw_curve_thickness_1)
 
 	terraformer::location loc{4.0f, 3.0f, 1.0f};
 
-	draw(img.pixels(), loc[0], loc[1], loc[2], 1.0f, [](float x, float y){
-		return x*x + y*y <= 1.0f? 1.0f : 0.0f;});
+	paint(img.pixels(),
+		 terraformer::paint_params{
+			.x = loc[0],
+			.y = loc[1],
+			.value = loc[2],
+			.brush_diameter = 1.0f
+		 });
 
 	for(uint32_t k = 0; k != img.height(); ++k)
 	{
@@ -32,8 +37,13 @@ TESTCASE(terraformer_draw_curve_thickness_2)
 
 	terraformer::location loc{4.0f, 3.0f, 1.0f};
 
-	draw(img.pixels(), loc[0], loc[1], loc[2], 2.0f, [](float x, float y){
-		return x*x + y*y <= 1.0f? 1.0f : 0.0f;});
+	paint(img.pixels(),
+		terraformer::paint_params{
+			.x = loc[0],
+			.y = loc[1],
+			.value = loc[2],
+			.brush_diameter = 2.0f
+		});
 
 	for(uint32_t k = 0; k != img.height(); ++k)
 	{
@@ -55,8 +65,13 @@ TESTCASE(terraformer_draw_curve_thickness_2_at_half_pixel)
 
 	loc += terraformer::displacement{0.5f, 0.5f, 0.0f};
 
-	draw(img.pixels(), loc[0], loc[1], loc[2], 2.0f, [](float x, float y){
-		return x*x + y*y <= 1.0f? 1.0f : 0.0f;});
+	paint(img.pixels(),
+		terraformer::paint_params{
+			.x = loc[0],
+			.y = loc[1],
+			.value = loc[2],
+			.brush_diameter = 2.0f
+		});
 
 	for(uint32_t k = 0; k != img.height(); ++k)
 	{
@@ -76,9 +91,13 @@ TESTCASE(terraformer_draw_curve_thickness_3)
 
 	terraformer::location loc{4.0f, 3.0f, 1.0f};
 
-	draw(img.pixels(), loc[0], loc[1], loc[2], 3.0f, [](float x, float y){
-		return x*x + y*y <= 1.0f? 1.0f : 0.0f;});
-
+	paint(img.pixels(),
+		terraformer::paint_params{
+			.x = loc[0],
+			.y = loc[1],
+			.value = loc[2],
+			.brush_diameter = 3.0f
+		});
 	for(uint32_t k = 0; k != img.height(); ++k)
 	{
 		for(uint32_t l = 0; l != img.width(); ++l)
@@ -97,9 +116,13 @@ TESTCASE(terraformer_draw_curve_thickness_4)
 
 	terraformer::location loc{4.0f, 3.0f, 1.0f};
 
-	draw(img.pixels(), loc[0], loc[1], loc[2], 4.0f, [](float x, float y){
-		return x*x + y*y <= 1.0f? 1.0f : 0.0f;});
-
+	paint(img.pixels(),
+		terraformer::paint_params{
+			.x = loc[0],
+			.y = loc[1],
+			.value = loc[2],
+			.brush_diameter = 4.0f
+		});
 	for(uint32_t k = 0; k != img.height(); ++k)
 	{
 		for(uint32_t l = 0; l != img.width(); ++l)
@@ -133,8 +156,13 @@ TESTCASE(terraformer_draw_curve_thickness_5)
 
 	terraformer::location loc{4.0f, 3.0f, 1.0f};
 
-	draw(img.pixels(), loc[0], loc[1], loc[2], 5.0f, [](float x, float y){
-		return x*x + y*y <= 1.0f? 1.0f : 0.0f;});
+	paint(img.pixels(),
+		terraformer::paint_params{
+			.x = loc[0],
+			.y = loc[1],
+			.value = loc[2],
+			.brush_diameter = 5.0f
+		});
 
 	for(uint32_t k = 0; k != img.height(); ++k)
 	{
@@ -169,8 +197,13 @@ TESTCASE(terraformer_draw_point_thickness_4_at_corner)
 
 	terraformer::location loc{0.0f, 0.0f, 1.0f};
 
-	draw(img.pixels(), loc[0], loc[1], loc[2], 4.0f, [](float x, float y){
-		return x*x + y*y <= 1.0f? 1.0f : 0.0f;});
+	paint(img.pixels(),
+		terraformer::paint_params{
+			.x = loc[0],
+			.y = loc[1],
+			.value = loc[2],
+			.brush_diameter = 4.0f
+		});
 
 	for(uint32_t k = 0; k != img.height(); ++k)
 	{
