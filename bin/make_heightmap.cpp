cd4e7c2b4e9d0c03215515b52b10bd0afd1c21fc
Torbjörn Rathsman <milasudril@gmail.com>
2023-02-15 21:47:10 +0100

    Create module for geomodels

--- bin/make_heightmap.cpp
+++ bin/make_heightmap.cpp
@@ -1,12 +1,7 @@
 //@	{"target":{"name":"make_heightmap.o"}}
 
-#include "lib/curve_tool/ridge_curve.hpp"
-#include "lib/curve_tool/noisy_drift.hpp"
-#include "lib/pixel_store/image.hpp"
-#include "lib/filters/curve_rasterizer.hpp"
+#include "lib/geomodels/steady_plate_collision_zone.hpp"
 #include "lib/pixel_store/image_io.hpp"
-#include "lib/filters/diffuser.hpp"
-#include "lib/execution/thread_pool.hpp"
 #include "lib/filters/coordinate_sampler.hpp"
 #include "lib/filters/gradient_tracer.hpp"
 #include "lib/filters/raycaster.hpp"
@@ -27,83 +22,6 @@ namespace terraformer
 	constexpr auto domain_area(dimensions const& dim)
 	{
 		return static_cast<double>(dim.width)*static_cast<double>(dim.height);
-	}
-
-	struct domain_boundary_conditions
-	{
-		float front_level;
-		float back_level;
-	};
-
-	struct massif_outline_descriptor
-	{
-		domain_boundary_conditions boundary;
-		main_ridge_params main_ridge;
-	};
-
-	template<class Rng>
-	void make_initial_heightmap(double_buffer<terraformer::grayscale_image>& buffers,
-		Rng&& rng,
-		float pixel_size,
-		massif_outline_descriptor const& heightmap_params)
-	{
-		auto const curve = generate(rng, pixel_size, heightmap_params.main_ridge);
-		auto const w = buffers.front().width();
-		auto const h = buffers.front().height();
-
-		terraformer::grayscale_image main_ridge{w, h};
-		draw(main_ridge.pixels(), curve, terraformer::line_segment_draw_params{
-			.value = 1.0f,
-			.scale = pixel_size
-		});
-
-		generate(buffers.back().pixels(), [
-				y_0 = heightmap_params.main_ridge.start_location[1]/pixel_size,
-				z_0 = heightmap_params.main_ridge.start_location[2],
-				h = static_cast<float>(h),
-				boundary = heightmap_params.boundary
-			](uint32_t, uint32_t y) {
-			auto const y_val = static_cast<float>(y);
-
-			auto const t1 = y_val/y_0;
-			auto const t2 = (y_val - y_0)/(h - y_0);
-
-			auto const z1 = std::lerp(boundary.back_level, z_0, t1);
-			auto const z2 = std::lerp(z_0, boundary.front_level, t2);
-
-			return t1 < 1.0f ? z1 : z2;
-		});
-		buffers.swap();
-
-		solve_bvp(buffers, terraformer::laplace_solver_params{
-			.tolerance = 1.0e-6f * heightmap_params.main_ridge.start_location[2],
-			.step_executor_factory = terraformer::thread_pool_factory{16},
-			.boundary = [
-				values = main_ridge,
-				front_back = heightmap_params.boundary
-			](uint32_t x, uint32_t y) {
-				if(y == 0)
-				{
-					return terraformer::dirichlet_boundary_pixel{
-						.weight=1.0f,
-						.value=front_back.back_level
-					};
-				}
-
-				if(y == values.height() - 1)
-				{
-					return terraformer::dirichlet_boundary_pixel{
-						.weight=1.0f,
-						.value=front_back.front_level
-					};
-				}
-
-				auto const val = values(x, y);
-				return val >= 0.5f ?
-					terraformer::dirichlet_boundary_pixel{1.0f, val}:
-					terraformer::dirichlet_boundary_pixel{0.0f, 0.0f};
-			},
-		});
 	}
 
 	struct domain_size_descriptor
@@ -154,7 +72,7 @@ namespace terraformer
 	struct landscape_descriptor
 	{
 		domain_size_descriptor domain_size;
-		massif_outline_descriptor initial_heightmap;
+		steady_plate_collision_zone_descriptor initial_heightmap;
 		geosimd::turn_angle north_offset;
 		weather_data_descriptor weather_data;
 
@@ -221,7 +139,7 @@ int main()
 	};
 
 	random_generator rng;
-	make_initial_heightmap(buffers, rng, pixel_size, params.initial_heightmap);
+	make_heightmap(buffers, rng, pixel_size, params.initial_heightmap);
 	store(buffers.front(), "after_laplace.exr");
 
 	terraformer::grayscale_image lit_surface{canvas_size.width, canvas_size.height};
