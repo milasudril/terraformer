f4d700f0fd624b949a3b266bcdfb9dcd3153013a
Torbjörn Rathsman <milasudril@gmail.com>
2023-02-26 08:54:47 +0100

    Move stuff to new file

---
+++ lib/geomodels/coordinate_transforms.hpp
@@ -0,0 +1,82 @@
+#ifndef TERRAFORMER_GEOMODELS_COORDINATETRANSFORMS_HPP
+#define TERRAFORMER_GEOMODELS_COORDINATETRANSFORMS_HPP
+
+#include "lib/common/spaces.hpp"
+#include "lib/common/span_2d.hpp"
+
+#include <geosimd/mat_4x4.hpp>
+
+namespace terraformer
+{
+	inline auto to_map_location(pixel_coordinates loc, span_2d_extents domain_size, double pixel_size)
+	{
+		hires_displacement const r{static_cast<double>(loc.x), static_cast<double>(loc.y), 0.0};
+		hires_displacement const size_vec{
+			static_cast<double>(domain_size.width),
+			static_cast<double>(domain_size.height),
+			0.0
+		};
+
+		return hires_origin + pixel_size*(r - 0.5*size_vec + hires_displacement{0.5, 0.5, 0.0});
+	}
+
+	inline auto to_colatitude(geosimd::turn_angle latitude)
+	{
+		return geosimd::rotation_angle{0x4000'0000} - latitude;
+	}
+
+	struct longcolat
+	{
+		geosimd::rotation_angle longitude;
+		geosimd::rotation_angle colatitude;
+	};
+
+	inline auto to_longcolat(hires_location loc,
+		double planet_radius,
+		geosimd::rotation_angle colat_offset)
+	{
+		auto const theta = colat_offset
+			+ geosimd::turn_angle{
+				geosimd::rad{loc[1]/planet_radius}
+			};
+		geosimd::rotation_angle const phi{
+			geosimd::rad{loc[0]/(planet_radius*sin(theta))}
+		};
+
+		return longcolat{
+			.longitude = phi,
+			.colatitude = theta
+		};
+	}
+
+
+	#if 0
+	inline auto planet_location(geosimd::turn_angle year, double distance_to_sun)
+	{
+		auto const cs = cossin<double>(year);
+		return hires_origin + distance_to_sun*hires_direction{cs, geosimd::dimension_tag<2>{}};
+	}
+
+	inline auto planet_rotation(geosimd::turn_angle spin_angle, geosimd::turn_angle tilt_angle)
+	{
+		geosimd::rotation<hires_geom_space> ret{};
+		ret.push(tilt_angle, geosimd::dimension_tag<1>{})
+			.push(spin_angle, geosimd::dimension_tag<2>{});
+		return ret;
+	}
+
+	template<class T>
+	concept planetary_tilt_modulation = requires(T x, geosimd::turn_angle t)
+	{
+		{x(t)} -> std::same_as<geosimd::rotation_angle>;
+	};
+
+	template<planetary_tilt_modulation TiltModulation>
+	inline auto planet_rotation(geosimd::turn_angle year, double spin_freq, TiltModulation&& tilt_mod)
+	{
+		return planet_rotation(spin_freq*year, std::forward<TiltModulation>(tilt_mod)(year));
+	}
+#endif
+}
+
+#endif
