652cb66a1c0747a2ad5cf659b047dfa6b6538379
Torbjörn Rathsman <milasudril@gmail.com>
2023-03-02 21:45:46 +0100

    Add wave sum function

---
+++ lib/curve_tool/wave_sum.hpp
@@ -0,0 +1,40 @@
+#ifndef TERRAFORMER_LIB_WAVESUM_HPP
+#define TERRAFORMER_LIB_WAVESUM_HPP
+
+#include <numbers>
+#include <cmath>
+
+namespace terraformer
+{
+	template<std::floating_point Precision>
+	class wave_sum
+	{
+	public:
+		struct params
+		{
+			Precision amplitude;
+			Precision base_frequency;
+			Precision frequency_ratio;
+			Precision phase_difference;
+			Precision mix;
+		};
+
+		explicit wave_sum(params const& params): m_params{params}
+		{
+		}
+
+		auto operator()(Precision x) const
+		{
+			auto const twopi = static_cast<Precision>(2.0)*std::numbers::pi_v<float>;
+			auto const f_0 = twopi*base_frequency;
+			auto const f_1 = f_0*m_params.frequency_ratio;
+ 			return m_params.amplitude*std::lerp(std::cos(f_0*x),
+				std::cos(f_1*x + m_params.phase), m_params.mix);
+		}
+
+	private:
+		params m_params;
+	};
+}
+
+#endif
