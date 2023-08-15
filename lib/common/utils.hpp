44b4b88b0b1f107c62de2b0bdb2b49dee920f92b
Torbjörn Rathsman <milasudril@gmail.com>
2023-08-15 18:06:35 +0200

    Improve accuracy in sine, at cost execution time
    Still twice as fast as std, yet much better resolution than previous commit

--- lib/common/utils.hpp
+++ lib/common/utils.hpp
@@ -37,8 +37,14 @@ namespace terraformer
 
 	constexpr auto approx_sine(float x)
 	{
-			auto const xi = 2.0f*(0.5f - mod(x/(2.0f*std::numbers::pi_v<float>), 1.0f));
-			return 4.0f*xi*(xi - 1.0f)*(xi + 1.0f)*(4.0f*xi*xi - 7.0f)/9.0f;
+	#if 0
+		auto const xi = 2.0f*(0.5f - mod(x/(2.0f*std::numbers::pi_v<float>), 1.0f));
+		return 4.0f*xi*(xi - 1.0f)*(xi + 1.0f)*(4.0f*xi*xi - 7.0f)/9.0f;
+	#else
+		auto const xi = 2.0f*(0.5f - mod(x/(2.0f*std::numbers::pi_v<float>), 1.0f));
+		return -(xi*(xi - 1.0f)*(xi + 1.0f)
+			*((13968.0f/31680.0f)*(xi*xi)*(xi*xi) - (63304.0f/31680.0f)*(xi*xi) + 99433.0f/31680.0f));
+	#endif
 	}
 
 	constexpr auto mod(int32_t x, int32_t denom)
--- lib/common/utils.test.cpp
+++ lib/common/utils.test.cpp
@@ -31,3 +31,13 @@ TESTCASE(terraformer_mod)
 		EXPECT_EQ(terraformer::mod(x, 4.0f), vals[k%16]);
    }
 }
+
+TESTCASE(terraformer_approx_sine)
+{
+	auto const dx = 2.0f*std::numbers::pi_v<float>/12.0f;
+	for(int k = -24; k != 24; ++k)
+	{
+		auto const x = dx*static_cast<float>(k);
+		printf("%.8g %.8g\n", x, terraformer::approx_sine(x));
+	}
+}
