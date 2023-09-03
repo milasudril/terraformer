7be1a2a5cfc44b52ea449bad167fe1978238495a
Torbjörn Rathsman <milasudril@gmail.com>
2023-09-03 19:44:52 +0200

    Add streamplot

--- experiments/levelcurves.py
+++ experiments/levelcurves.py
@@ -10,13 +10,18 @@ if inp:
 	yres = spec.height
 	nchannels = spec.nchannels
 	pixels = inp.read_image(0, 0, 0, nchannels, 'float')
-	print(pixels[:, :, 0].ndim)
+	pixels = numpy.flipud(pixels[:, :, 0])
 	inp.close()
 	t = numpy.linspace(0, 16, 17)/16
 	exp = t*13 + (1 - t)*9
 	levels = numpy.power(2, exp)
-	matplotlib.pyplot.contour(numpy.flipud(pixels[:, :, 0]), levels, extent = [0, 49152, 0, 49252], linewidths=0.75)
+
+	y, x = 48*numpy.mgrid[0:yres:16, 0:xres:16]
+	dy, dx = numpy.gradient(pixels[::16, ::16])
+
+	matplotlib.pyplot.contour(pixels, levels, extent = [0, 49152, 0, 49252], linewidths=0.75)
 	matplotlib.pyplot.colorbar()
+	matplotlib.pyplot.streamplot(x, y, -dx, -dy, linewidth=0.75)
 	ax = matplotlib.pyplot.gca()
 	ax.set_aspect('equal', adjustable='box')
 	matplotlib.pyplot.show()
