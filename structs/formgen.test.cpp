24e1773fed95074008070135539740e5a924d4cd
Torbjörn Rathsman <milasudril@gmail.com>
2023-09-09 11:54:38 +0200

    Introduce interval types

--- structs/formgen.test.cpp
+++ structs/formgen.test.cpp
@@ -3,6 +3,7 @@
 #include <type_traits>
 #include <string>
 #include <charconv>
+#include <stdexcept>
 
 #include <limits>
 #include <functional>
@@ -17,23 +18,82 @@ struct field
 };
 
 template<class T>
-requires std::is_arithmetic_v<T>
-struct string_converter
-{
+struct open_open_interval{
+	using value_type = T;
 	T min;
 	T max;
+};
 
-	static std::string to_string(T value)
+template<class T>
+bool within(open_open_interval<T> range, T val)
+{ return val > range.min && val < range.max; }
+
+template<class T>
+struct closed_open_interval{
+	using value_type = T;
+	T min;
+	T max;
+};
+
+template<class T>
+bool within(closed_open_interval<T> range, T val)
+{ return val >= range.min && val < range.max; }
+
+template<class T>
+struct open_closed_interval{
+	using value_type = T;
+	T min;
+	T max;
+};
+
+template<class T>
+bool within(open_closed_interval<T> range, T val)
+{ return val > range.min && val <= range.max; }
+
+template<class T>
+struct closed_closed_interval{
+	using value_type = T;
+	T min;
+	T max;
+};
+
+template<class T>
+bool within(closed_closed_interval<T> range, T val)
+{ return val >= range.min && val <= range.max; }
+
+template<class ValidRange>
+requires std::is_arithmetic_v<typename ValidRange::value_type>
+struct string_converter
+{
+	using deserialized_type = typename ValidRange::value_type;
+	ValidRange range;
+
+	static std::string to_string(deserialized_type value)
 	{
 		std::array<char, 32> buffer{};
 		std::to_chars(std::begin(buffer), std::end(buffer), value);
 		return std::string{std::data(buffer)};
 	}
 
-	T from_string(std::string_view str)
+	deserialized_type from_string(std::string_view str)
 	{
+		deserialized_type val{};
+		auto const res = std::from_chars(std::begin(str), std::end(str), val);
+		switch(res.ec)
+		{
+			case std::errc{}:
+				if(within(range, val))
+				{ return val; }
+				throw std::runtime_error{"Input value is out of range"};
 
-		return min;
+			case std::errc::result_out_of_range:
+				throw std::runtime_error{"Input value is out of range"};
+
+			default:
+				throw std::runtime_error{"Expected a number"};
+		}
+
+		return range.min;
 	}
 };
 
@@ -55,8 +115,10 @@ void bind(Form& form, domain_size& dom_size)
 			.description = "Sets the width of the domain",
 			.widget = typename Form::textbox{
 				.value_converter = string_converter{
-					.min = 0.0f,
-					.max = std::numeric_limits<float>::infinity()
+					.range = open_open_interval{
+						.min = 0.0f,
+						.max = std::numeric_limits<float>::infinity(),
+					}
 				},
 				.binding = std::ref(dom_size.width)
 			}
@@ -70,8 +132,10 @@ void bind(Form& form, domain_size& dom_size)
 			.description = "Sets the width of the domain",
 			.widget = typename Form::textbox{
 				.value_converter = string_converter{
-					.min = 0.0f,
-					.max = std::numeric_limits<float>::infinity()
+					.range = open_open_interval{
+						.min = 0.0f,
+						.max = std::numeric_limits<float>::infinity(),
+					}
 				},
 				.binding = std::ref(dom_size.height)
 			}
@@ -85,8 +149,10 @@ void bind(Form& form, domain_size& dom_size)
 			.description = "Sets the number of scanlines in the generated images",
 			.widget = typename Form::textbox{
 				.value_converter = string_converter{
-					.min = 1,
-					.max = 8192
+					.range = closed_closed_interval{
+						.min = 1,
+						.max = 8192,
+					}
 				},
 				.binding = std::ref(dom_size.scanline_count)
 			}
