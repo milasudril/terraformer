5f7889ac9df1d52db23972c323564dc2318c98b6
Torbjörn Rathsman <milasudril@gmail.com>
2023-09-30 10:28:38 +0200

    Add level curves overlay

--- ui/form.cpp
+++ ui/form.cpp
@@ -34,32 +34,27 @@ namespace
 
 void terraformer::topographic_map_renderer::upload(grayscale_image const& img)
 {
-#if 0
-	auto const img_to_present = generate_level_curves(
-		terraformer::resize(img, image_resize_description{
+	auto const fitted_image = terraformer::resize(img,
+		image_resize_description{
 			.output_width = static_cast<uint32_t>(m_image_view->width()),
 			.output_height = static_cast<uint32_t>(m_image_view->height())
-		}),
-		posterization_description{
-		.levels = 16
 		}
 	);
-#else
-	auto const img_to_present = apply_colormap(
-		terraformer::resize(img, image_resize_description{
-			.output_width = static_cast<uint32_t>(m_image_view->width()),
-			.output_height = static_cast<uint32_t>(m_image_view->height())
-		}),
-		earth_colormap
-	);
-#endif
 
-	basic_image<output_pixel> img_srgb{img_to_present.width(), img_to_present.height()};
+	auto const level_curves = generate_level_curves(fitted_image,
+		posterization_description{
+			.levels = 16
+		}
+	);
+
+	auto const mapped_intensity = apply_colormap(fitted_image, earth_colormap);
+
+	basic_image<output_pixel> img_srgb{fitted_image.width(), fitted_image.height()};
 	for(uint32_t y = 0; y != img_srgb.height(); ++y)
 	{
 		for(uint32_t x = 0; x != img_srgb.width(); ++x)
 		{
-			auto const output_value = img_to_present(x, y);
+			auto const output_value = mapped_intensity(x, y)*(1.0f - 0.5f*level_curves(x, y));
 
 			img_srgb(x, y) = output_pixel{
 				.b = static_cast<uint8_t>(255.0f*std::pow(output_value.blue(), 1.0f/2.2f)),
