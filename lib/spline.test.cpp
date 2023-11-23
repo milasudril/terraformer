90d6a460d6eb9698bd6c37b4900ae48827a10fa4
Torbjörn Rathsman <milasudril@gmail.com>
2023-11-23 21:41:28 +0100

    Introduce spline function

--- lib/spline.test.cpp
+++ lib/spline.test.cpp
@@ -1,4 +1,4 @@
-//@	{"target":{"name":"utils.test"}}
+//@	{"target":{"name":"spline.test"}}
 
 #include "./spline.hpp"
 
@@ -6,10 +6,27 @@
 
 TESTCASE(terraformer_smoothstep)
 {
-	auto const dx = 1.0f/4.0f;
-	for(int k = -16; k <= 16; ++k)
-	{
-		auto const x = dx*static_cast<float>(k);
-		printf("%.8g   %.8g\n", x, terraformer::smoothstep(x));
-	}
+	EXPECT_EQ(terraformer::smoothstep(-2.0f), 0.0f);
+	EXPECT_EQ(terraformer::smoothstep(-1.0f), 0.0f);
+	EXPECT_EQ(terraformer::smoothstep(0.0f), 0.5f);
+	EXPECT_EQ(terraformer::smoothstep(1.0f), 1.0f);
+	EXPECT_EQ(terraformer::smoothstep(2.0f), 1.0f);
+}
+
+TESTCASE(terraformer_spline)
+{
+	terraformer::spline_control_point a{
+		.y = 0.0f,
+		.ddx = -1.0f
+	};
+
+	terraformer::spline_control_point b{
+		.y = 1.0f,
+		.ddx = -2.0f
+	};
+
+	EXPECT_EQ(spline(0.0f, a, b), a.y);
+	EXPECT_EQ(spline(1.0f, a, b), b.y);
+
+	// TODO: Check endpoint derivatives somehow
 }
