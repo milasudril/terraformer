fd28d74292f3e25d8581f7493448302133cd5645
Torbjörn Rathsman <milasudril@gmail.com>
2023-11-24 19:41:56 +0100

    Move spline to interpolation lib

--- lib/spline.hpp
+++ lib/spline.hpp
@@ -5,36 +5,11 @@
 
 namespace terraformer
 {
-//	a=-(y-B-A)/4,b=-(A-B)/4,c=(3*y-B-A)/4,d=(2*y-B+A)/4
-
 	constexpr auto smoothstep(float x)
 	{
 		x = std::clamp(x, -1.0f, 1.0f);
 		return (2.0f - x)*(x + 1.0f)*(x + 1.0f)/4.0f;
 	}
-
-	struct spline_control_point
-	{
-		float y;
-		float ddx;
-	};
-
-	constexpr auto spline(float x, spline_control_point a,  spline_control_point b)
-	{
-		auto const y_0 = a.y;
-		auto const y_1 = b.y;
-		auto const A = a.ddx;
-		auto const B = b.ddx;
-
-		// a=B+A-2*y[1]+2*y[0],b=-B-2*A+3*y[1]-3*y[0],c=A,d=y[0]
-		auto const cubic = B + A - 2.0f*(y_1 - y_0);
-		auto const quadratic = -B - 2.0f*A + 3.0f*(y_1-y_0);
-		auto const linear = A;
-		auto const constant = y_0;
-
-		return cubic*x*x*x + quadratic*x*x + linear*x + constant;
-	}
-
 }
 
 #endif
--- lib/spline.test.cpp
+++ lib/spline.test.cpp
@@ -12,21 +12,3 @@ TESTCASE(terraformer_smoothstep)
 	EXPECT_EQ(terraformer::smoothstep(1.0f), 1.0f);
 	EXPECT_EQ(terraformer::smoothstep(2.0f), 1.0f);
 }
-
-TESTCASE(terraformer_spline)
-{
-	terraformer::spline_control_point a{
-		.y = 0.0f,
-		.ddx = -1.0f
-	};
-
-	terraformer::spline_control_point b{
-		.y = 1.0f,
-		.ddx = -2.0f
-	};
-
-	EXPECT_EQ(spline(0.0f, a, b), a.y);
-	EXPECT_EQ(spline(1.0f, a, b), b.y);
-
-	// TODO: Check endpoint derivatives somehow
-}
