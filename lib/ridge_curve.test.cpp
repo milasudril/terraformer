a25427013fe955943f723a5be22f78b21d4afecb
Torbjörn Rathsman <milasudril@gmail.com>
2023-12-02 18:01:23 +0100

    Use DFT to set verdict on curve shape

--- lib/ridge_curve.test.cpp
+++ lib/ridge_curve.test.cpp
@@ -2,6 +2,8 @@
 
 #include "./ridge_curve.hpp"
 
+#include "lib/filters/dft_engine.hpp"
+
 #include <testfwk/testfwk.hpp>
 
 TESTCASE(terraformer_ridge_curve_generate)
@@ -16,4 +18,21 @@ TESTCASE(terraformer_ridge_curve_generate)
 	terraformer::random_generator rng;
 	auto const res = generate(curve, rng, 49152, 1.0f);
 	EXPECT_EQ(std::size(res), 49152);
+
+	std::vector<std::complex<double>> input(std::size(res));
+	std::transform(std::begin(res), std::end(res), std::begin(input), [](auto val){
+		return static_cast<double>(val[1]);
+	});
+	std::vector<std::complex<double>> output(std::size(res));
+	get_plan(std::size(res), terraformer::dft_direction::forward).execute(std::data(input), std::data(output));
+
+	auto max_iter = std::max_element(
+		std::begin(output),
+		std::begin(output) + std::size(output)/2,
+		[](auto a, auto b) {
+			return std::abs(a) < std::abs(b);
+		}
+	);
+
+	EXPECT_EQ(max_iter - std::begin(output), 49152/6144);
 }
