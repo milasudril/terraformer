6530b78d6b5d7d94c0db29103be28f01c2439773
Torbjörn Rathsman <milasudril@gmail.com>
2023-12-02 11:38:16 +0100

    Adding stuff to ridge curve

--- lib/ridge_curve.hpp
+++ lib/ridge_curve.hpp
@@ -1,12 +1,20 @@
 #ifndef TERRAFORMER_RIDGECURVE_HPP
 #define TERRAFORMER_RIDGECURVE_HPP
 
+#include "lib/modules/dimensions.hpp"
+
 namespace terraformer
 {
 	struct ridge_curve_description
 	{
-
+		horizontal_offset initial_value;
+		horizontal_amplitude amplitude;
+		bool flip_direction{};
+		bool invert_displacement{};
+		domain_length wavelength;
 	};
-};
+
+	std::vector<displacement> generate(ridge_curve_description const& src, float dx);
+}
 
 #endif
