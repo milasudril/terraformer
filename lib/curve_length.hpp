6273acc810304a3004108634353e37d23c7c7f0b
Torbjörn Rathsman <milasudril@gmail.com>
2024-01-06 11:39:02 +0100

    Add function that computes distance between point and curve

--- lib/curve_length.hpp
+++ lib/curve_length.hpp
@@ -7,9 +7,12 @@ namespace terraformer
 {
 	inline auto curve_length(std::span<location const> locs)
 	{
-		return fold_over_line_segments(locs, [](auto lineseg, float prev){
-			return prev + length(lineseg);
-		},0.0f);
+		return fold_over_line_segments(
+			locs,
+			[](auto lineseg, auto ... vals){
+				return (vals + ... + length(lineseg));
+			}
+		);
 	}
 }
 
--- lib/line_segment.hpp
+++ lib/line_segment.hpp
@@ -17,21 +17,46 @@ namespace terraformer
 	inline auto length(line_segment seg)
 	{ return distance(seg.a, seg.b); }
 
-	template<class Func, class Init, class ... Args>
-	auto fold_over_line_segments(std::span<location const> locs, Func f, Init&& init, Args... args)
+	inline auto length_squared(line_segment seg)
+	{ return distance_squared(seg.a, seg.b); }
+
+	inline auto distance(line_segment seg, location loc)
 	{
-		using ret_type = std::result_of_t<Func(line_segment, Init, Args...)>;
-		static_assert(std::is_same_v<ret_type, Init>);
+		auto const l2 = length_squared(seg);
+
+		if(l2 == 0.0f) [[unlikely]]
+		{ return distance(seg.a, loc); }
+
+		auto const t = std::max(0.0f, std::min(1.0f, inner_product(loc - seg.a, seg.b - seg.a) / l2));
+		auto const proj = seg.a + t*(seg.b - seg.a);
+
+		return distance(loc, proj);
+	}
+
+	template<class Func, class ... Args>
+	auto fold_over_line_segments(std::span<location const> locs, Func f, Args... args)
+	{
+		using ret_type = std::result_of_t<Func(line_segment, Args...)>;
 
 		if(std::size(locs) < 2)
 		{ return ret_type{}; }
 
-		auto ret = f(line_segment{locs[0], locs[1]}, std::forward<Init>(init), args...);
+		auto ret = f(line_segment{locs[0], locs[1]}, args...);
 
 		for(size_t k = 2; k != std::size(locs); ++k)
-		{ ret = f(line_segment{locs[k - 1], locs[k]}, std::move(ret), args...); }
+		{ ret = f(line_segment{locs[k - 1], locs[k]}, args..., std::move(ret)); }
 
 		return ret;
+	}
+
+	inline auto distance(std::span<location const> locs, location loc)
+	{
+		return fold_over_line_segments(locs, [](auto seg, auto loc, auto ... old_distance) {
+			if constexpr(sizeof...(old_distance) == 0)
+			{ return distance(seg, loc); }
+			else
+			{ return std::min(distance(seg, loc), old_distance...); }
+		}, loc);
 	}
 }
 
