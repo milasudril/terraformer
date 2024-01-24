f0b8c91bcff5fba3c60572dd9d1661c6eb507d76
Torbjörn Rathsman <milasudril@gmail.com>
2024-01-24 21:48:10 +0100

    Implement bfs for each

--- lib/generators/ridge_tree_generator/ridge_tree.hpp
+++ lib/generators/ridge_tree_generator/ridge_tree.hpp
@@ -5,13 +5,36 @@
 
 #include "./ridge_tree_branch.hpp"
 
+#include <deque>
+
 namespace terraformer
 {
 	struct ridge_tree
 	{
 		ridge_tree_branch stem;
-		std::vector<ridge_tree> children;
+		std::vector<ridge_tree> left_children;
+		std::vector<ridge_tree> right_children;
 	};
+
+	template<class Function, class ... Args>
+	void for_each_bfs(ridge_tree const& tree, Fucntion&& f, Args... args)
+	{
+		std::queue<std::reference_wrapper<ridge_tree const>> nodes;
+		nodes.push(tree);
+		while(!nodes.empty())
+		{
+			auto next = nodes.front();
+			f(next.get(), args...);
+
+			for(auto const& child: next.get().left_children)
+			{ nodes.push(child); }
+
+			for(auto const& child: next.get().right_children)
+			{ nodes.push(child); }
+
+			nodes.pop();
+		}
+	}
 
 	ridge_tree generate(size_t max_depth);
 }
