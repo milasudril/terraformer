94cb0f05080e9bd89b4b33bae95ff4b6dc39a24c
Torbjörn Rathsman <milasudril@gmail.com>
2024-01-27 10:14:58 +0100

    More work on ridge_tree

--- lib/generators/ridge_tree_generator/ridge_tree.cpp
+++ lib/generators/ridge_tree_generator/ridge_tree.cpp
@@ -2,25 +2,14 @@
 
 #include "./ridge_tree.hpp"
 
-#include "lib/common/tree_generator.hpp"
-
-std::optional<terraformer::ridge_tree_node> terraformer::ridge_tree_builder::make_node()
-{
-	return terraformer::ridge_tree_node{
-		.level = 0
-	};
-}
-
-std::optional<terraformer::ridge_tree_node> terraformer::ridge_tree_builder::make_node(ridge_tree_node const&)
-{
-	return std::nullopt;
-}
-
-void terraformer::ridge_tree_builder::save_node(ridge_tree_node&&){}
-
-terraformer::ridge_tree terraformer::generate_tree()
+terraformer::ridge_tree terraformer::generate_tree(
+	std::span<ridge_curve_description const> curve_levels,
+	random_generator
+)
 {
 	ridge_tree ret{};
-	generate_tree_bfs(ridge_tree_builder{ret});
+	if(std::size(curve_levels) == 0)
+	{ return ret; }
+
 	return ret;
 }
--- lib/generators/ridge_tree_generator/ridge_tree.hpp
+++ lib/generators/ridge_tree_generator/ridge_tree.hpp
@@ -3,40 +3,30 @@
 #ifndef TERRAFORMER_RIDGE_TREE_HPP
 #define TERRAFORMER_RIDGE_TREE_HPP
 
-#include "lib/common/array_tuple.hpp"
-#include "lib/common/spaces.hpp"
+#include "./ridge_curve.hpp"
+
+#include "lib/curve_tools/displace.hpp"
+#include "lib/common/rng.hpp"
+
+#include <vector>
 
 namespace terraformer
 {
-	enum class ridge_tree_branch_side{left, right};
-
-	constexpr auto ridge_tree_locations = 0;
-	constexpr auto ridge_tree_normals = 1;
-	constexpr auto ridge_tree_levels = 2;
-	constexpr auto ridge_tree_sides = 3;
-	using ridge_tree = array_tuple<location, direction, size_t, ridge_tree_branch_side>;
-
-	struct ridge_tree_node
+	struct ridge_tree_branch
 	{
 		size_t level;
+		displaced_curve curve;
 	};
 
-	class ridge_tree_builder
+	struct ridge_tree : std::vector<ridge_tree_branch>
 	{
-	public:
-		explicit ridge_tree_builder(ridge_tree& output):m_output{output}{}
-
-		std::optional<ridge_tree_node> make_node();
-
-		std::optional<ridge_tree_node> make_node(ridge_tree_node const&);
-
-		void save_node(ridge_tree_node&& node);
-
-	private:
-		std::reference_wrapper<ridge_tree> m_output;
+		using std::vector<ridge_tree_branch>::vector;
 	};
 
-	ridge_tree generate_tree();
+	ridge_tree generate_tree(
+		std::span<ridge_curve_description const> curve_levels,
+		random_generator rng
+	);
 }
 
 #endif
