979c1e7b3180a87ec30c12837f626401d1a86a25
Torbjörn Rathsman <mialsudril@gmail.com>
2024-02-04 09:39:58 +0100

    Add arithmetic operators to array_size

--- lib/array_classes/array_index.hpp
+++ lib/array_classes/array_index.hpp
@@ -3,6 +3,7 @@
 
 #include <type_traits>
 #include <cstddef>
+#include <stdexcept>
 
 namespace terraformer
 {
@@ -10,15 +11,53 @@ namespace terraformer
 	class array_size
 	{
 	public:
-		explicit array_size(Rep value): m_value{value}{}
+		constexpr array_size() = default;
+				
+		constexpr explicit array_size(Rep value): m_value{value}{}
 
 		constexpr auto get() const { return m_value; }
 
 		constexpr auto operator<=>(array_size const&) const = default;
+		
+		constexpr array_size& operator+=(array_size other)
+		{
+			if(__builtin_add_overflow(m_value, other.get(), &m_value))
+			{ throw std::runtime_error{"Arithmetic overflow"}; }
+			
+			return *this;
+		}
+		
+		constexpr array_size& operator-=(array_size other)
+		{
+			if(__builtin_sub_overflow(m_value, other.get(), &m_value))
+			{ throw std::runtime_error{"Arithmetic overflow"}; }
+			
+			return *this;
+		}
+
+		constexpr array_size& operator*=(Rep factor)
+		{
+			if(__builtin_mul_overflow(m_value, factor, &m_value))
+			{ throw std::runtime_error{"Arithmetic overflow"}; }
+			
+			return *this;
+		}
 
 	private:
-		Rep m_value;
+		Rep m_value{};
 	};
+	
+	template<class T, class Rep>
+	constexpr auto operator+(array_size<T, Rep> a, array_size<T, Rep> b)
+	{ return a += b; }
+
+	template<class T, class Rep>
+	constexpr auto operator-(array_size<T, Rep> a, array_size<T, Rep> b)
+	{ return a -= b; }
+	
+	template<class T, class Rep>
+	constexpr auto operator*(Rep c, array_size<T, Rep> a)
+	{ return a *= c; }
 
 	template<class T, class Rep = size_t>
 	class array_index
