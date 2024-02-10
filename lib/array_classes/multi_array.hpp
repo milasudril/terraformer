450ed36516047d6acf1aa7bc6c5885723325d5e7
Torbjörn Rathsman <milasudril@gmail.com>
2024-02-10 11:50:00 +0100

    Copy single array to multi_array

---
+++ lib/array_classes/multi_array.hpp
@@ -0,0 +1,140 @@
+#ifndef TERRAFORMER_MULTI_ARRAY_HPP
+#define TERRAFORMER_MULTI_ARRAY_HPP
+
+#include "./memory_block.hpp"
+#include "./span.hpp"
+#include "lib/common/tuple.hpp"
+
+namespace terraformer
+{
+	template<class T>
+	class multi_array
+	{
+	public:
+		multi_array() = default;
+
+		explicit multi_array(array_size<T> size)
+		{ resize(size); }
+
+		multi_array(multi_array&& other) noexcept:
+			m_storage{std::exchange(other.m_storage, memory_block{})},
+			m_size{std::exchange(other.m_size, array_size<T>{})},
+			m_capacity{std::exchange(other.m_capacity, array_size<T>{})}
+		{ }
+
+		multi_array(multi_array const& other):
+			m_storage{make_byte_size(other.capacity())},
+			m_size{other.m_size},
+			m_capacity{other.m_capacity}
+		{ std::uninitialized_copy_n(other.begin(), m_size.get(), m_storage.template interpret_as<T>()); }
+
+		multi_array& operator=(multi_array&& other) noexcept
+		{
+			clear();
+			m_storage = std::exchange(other.m_storage, memory_block{});
+			m_size = std::exchange(other.m_size, array_size<T>{});
+			m_capacity = std::exchange(other.m_capacity, array_size<T>{});
+			return *this;
+		}
+
+		multi_array& operator=(multi_array const& other) = delete;
+
+		~multi_array()
+		{ clear(); }
+
+		constexpr auto first_element_index() const
+		{ return array_index<T>{}; }
+
+		auto size() const
+		{ return m_size; }
+
+		auto capacity() const
+		{ return m_capacity; }
+
+		auto data()
+		{ return m_storage.interpret_as<T>(); }
+
+		auto data() const
+		{ return m_storage.interpret_as<T const>(); }
+
+		auto begin()
+		{ return data(); }
+
+		auto begin() const
+		{ return data(); }
+
+		auto end()
+		{ return begin() + size().get(); }
+
+		auto end() const
+		{ return begin() + size().get(); }
+
+		void reserve(array_size<T> new_capacity)
+		{
+			if(new_capacity > m_capacity)
+			{
+				memory_block new_storage{make_byte_size(new_capacity)};
+				std::uninitialized_move(begin(), end(), new_storage.interpret_as<T>());
+				std::destroy(begin(), end());
+				m_storage = std::move(new_storage);
+				m_capacity = new_capacity;
+			}
+		}
+
+		void push_back(T&& elem)
+		{
+			auto new_size = m_size + terraformer::array_size<T>{1};
+			if(new_size > m_capacity)
+			{ reserve(std::max(terraformer::array_size<T>{8}, static_cast<size_t>(2)*capacity())); }
+			std::construct_at(m_storage.interpret_as<T>() + m_size.get(), std::move(elem));
+			m_size = new_size;
+		}
+
+		void clear() noexcept
+		{
+			std::destroy(begin(), end());
+			m_size = array_size<T>{};
+		}
+
+		void resize(array_size<T> new_size)
+		{
+			if(new_size < m_size)
+			{
+				truncate_from(array_index<T>{new_size.get()});
+				m_size = new_size;
+				return;
+			}
+
+			if(new_size > m_size)
+			{
+				if(new_size > m_capacity)
+				{ reserve(new_size); }
+				std::uninitialized_default_construct_n(end(), (new_size - m_size).get());
+				m_size = new_size;
+				return;
+			}
+		}
+
+		void truncate_from(array_index<T> index)
+		{ std::destroy(begin() + index.get(), end()); }
+
+		auto& operator[](array_index<T> index)
+		{ return deref(data(), index); }
+
+		auto& operator[](array_index<T> index) const
+		{ return deref(data(), index); }
+
+		operator span<T>()
+		{ return span{begin(), end()}; }
+
+		operator span<T const>()
+		{ return span{end(), end()}; }
+
+	private:
+		memory_block m_storage{};
+		array_size<T> m_size{};
+ 		array_size<T> m_capacity{};
+	};
+}
+
+#endif
---
+++ lib/array_classes/multi_array.test.cpp
@@ -0,0 +1,172 @@
+//@	{"target":{"name":"multi_array.test"}}
+
+#include "./multi_array.hpp"
+
+#include <cassert>
+#include <testfwk/testfwk.hpp>
+#include <testfwk/lifetime_checker.hpp>
+
+namespace
+{
+	using default_constructible_type = testfwk::lifetime_checker<int>;
+
+	class holder
+	{
+	public:
+		explicit holder(int value):m_value{value}
+		{}
+
+		auto value() const
+		{ return m_value; }
+
+	private:
+		int m_value;
+	};
+
+	using no_default_constructible_type = testfwk::lifetime_checker<holder>;
+}
+
+TESTCASE(terraformer_multi_array_reseve_on_empty)
+{
+	terraformer::multi_array<no_default_constructible_type> array;
+	REQUIRE_EQ(array.size(), terraformer::array_size<no_default_constructible_type>{});
+	REQUIRE_EQ(array.capacity(), terraformer::array_size<no_default_constructible_type>{});
+	REQUIRE_EQ(array.begin(), array.end());
+
+	array.reserve(terraformer::array_size<no_default_constructible_type>(5));
+
+	EXPECT_EQ(array.size(), terraformer::array_size<no_default_constructible_type>{});
+	EXPECT_EQ(array.capacity(), terraformer::array_size<no_default_constructible_type>{5});
+	EXPECT_EQ(array.begin(), array.end());
+}
+
+TESTCASE(terraformer_multi_array_push_back)
+{
+	terraformer::multi_array<no_default_constructible_type> array;
+
+	for(int k = 0; k != 16; ++k)
+	{
+		no_default_constructible_type::expect_ctor();
+		if(static_cast<size_t>(k) == array.capacity().get())
+		{ no_default_constructible_type::expect_move_ctor(array.capacity().get()); }
+		no_default_constructible_type::expect_move_ctor();
+		array.push_back(no_default_constructible_type{k});
+	}
+
+	for(auto k = array.first_element_index(); k != std::size(array); ++k)
+	{ EXPECT_EQ(static_cast<size_t>(static_cast<holder>(array[k]).value()), k.get()); }
+
+	EXPECT_EQ(std::size(array), terraformer::array_size<no_default_constructible_type>{16});
+
+	{
+		int k = 0;
+		for(auto& item : array)
+		{
+			EXPECT_EQ(static_cast<holder>(item).value(), k);
+			++k;
+		}
+		EXPECT_EQ(k, 16);
+	}
+
+	{
+		auto span = static_cast<terraformer::span<no_default_constructible_type>>(array);
+		for(auto k = array.first_element_index(); k != std::size(span); ++k)
+		{ EXPECT_EQ(static_cast<size_t>(static_cast<holder>(array[k]).value()), k.get()); }
+	}
+}
+
+TESTCASE(terraformer_multi_array_resize_grow_and_shrink)
+{
+	terraformer::multi_array<default_constructible_type> array;
+
+	for(int k = 0; k != 4; ++k)
+	{
+		default_constructible_type::expect_ctor();
+		default_constructible_type::expect_move_ctor();
+		array.push_back(default_constructible_type{k});
+	}
+
+	for(int k = 0; k != 4; ++k)
+	{ default_constructible_type::expect_move_ctor(); }
+	for(int k = 0; k != 12; ++k)
+	{ default_constructible_type::expect_ctor(); }
+	array.resize(terraformer::array_size<default_constructible_type>{16});
+
+	EXPECT_EQ(array.capacity().get(), 16);
+	EXPECT_EQ(array.size().get(), 16);
+	for(auto k = array.first_element_index() + 4; k != std::size(array); ++k)
+	{ EXPECT_EQ(static_cast<int>(array[k]), 0); }
+
+	array.resize(terraformer::array_size<default_constructible_type>{8});
+	EXPECT_EQ(array.size().get(), 8);
+}
+
+TESTCASE(terraformer_multi_array_move)
+{
+	terraformer::multi_array<no_default_constructible_type> array;
+
+	no_default_constructible_type::expect_ctor(4);
+	no_default_constructible_type::expect_move_ctor(4);
+	array.push_back(1);
+	array.push_back(2);
+	array.push_back(3);
+	array.push_back(4);
+
+	auto const old_ptr = std::data(array);
+	auto other = std::move(array);
+
+	EXPECT_EQ(std::size(array).get(), 0);
+	EXPECT_EQ(std::size(other).get(), 4);
+	EXPECT_EQ(std::data(other), old_ptr);
+	EXPECT_EQ(std::data(array), nullptr);
+}
+
+TESTCASE(terraformer_multi_array_copy)
+{
+	terraformer::multi_array<no_default_constructible_type> array;
+
+	no_default_constructible_type::expect_ctor(4);
+	no_default_constructible_type::expect_move_ctor(4);
+	array.push_back(1);
+	array.push_back(2);
+	array.push_back(3);
+	array.push_back(4);
+
+	auto const old_ptr = std::data(array);
+
+	no_default_constructible_type::expect_copy_ctor(4);
+	auto const other = array;
+
+	EXPECT_EQ(std::size(array).get(), 4);
+	EXPECT_EQ(std::size(other).get(), 4);
+	EXPECT_NE(std::data(other), old_ptr);
+	EXPECT_NE(std::data(other), nullptr);
+	EXPECT_EQ(std::data(array), old_ptr);
+}
+
+TESTCASE(terraformer_multi_array_move_assign)
+{
+		terraformer::multi_array<no_default_constructible_type> array;
+
+		no_default_constructible_type::expect_ctor(4);
+		no_default_constructible_type::expect_move_ctor(4);
+		array.push_back(1);
+		array.push_back(2);
+		array.push_back(3);
+		array.push_back(4);
+
+		terraformer::multi_array<no_default_constructible_type> other;
+		no_default_constructible_type::expect_ctor(3);
+		no_default_constructible_type::expect_move_ctor(3);
+		other.push_back(5);
+		other.push_back(6);
+		other.push_back(7);
+
+		auto new_ptr = std::data(other);
+
+		array = std::move(other);
+		EXPECT_EQ(std::data(array), new_ptr);
+		EXPECT_EQ(std::data(other), nullptr);
+		EXPECT_EQ(std::size(array).get(), 3);
+		EXPECT_EQ(std::size(other).get(), 0);
+}
