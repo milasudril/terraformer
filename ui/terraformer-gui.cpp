ae1d0aa54817c7fb0495dcfa9fcf1749b7bc6479
Torbjörn Rathsman <milasudril@gmail.com>
2024-03-25 18:34:22 +0100

    I am going to do this the hard way

--- ui/terraformer-gui.cpp
+++ ui/terraformer-gui.cpp
@@ -4,7 +4,6 @@
 
 #include "./renderer/gl_surface_configuration.hpp"
 #include "./wsapi/native_window.hpp"
-#include "./widgets/client_window.hpp"
 
 namespace
 {
@@ -17,34 +16,10 @@ namespace
 		{fb_size = size;}
 
 		bool operator()(
-			terraformer::ui::wsapi::native_window<terraformer::ui::renderer::gl_surface_configuration>& viewport,
-			terraformer::ui::widgets::toolkit_instance& tk
+			terraformer::ui::wsapi::native_window<terraformer::ui::renderer::gl_surface_configuration>& viewport
 		)
 		{
-			tk.prepare_frame();
-			{
-				terraformer::ui::widgets::client_window window{
-					tk,
-					nullptr,
-					terraformer::ui::widgets::window_configuration{
-						.geometry{
-							.loc_x = 0.0f,
-							.loc_y = 0.0f,
-							.width = static_cast<float>(fb_size.width),
-							.height = static_cast<float>(fb_size.height)
-						}
-					}
-				};
-
-				for(size_t k = 0; k != 10; ++k)
-				{
-					ImGui::Button(std::to_string(k).append(" A Button sdajkflase foeisaj pseaifj espaof jesapf ").c_str());
-				}
-			}
-
 			glClear(GL_COLOR_BUFFER_BIT);
-			tk.finalize_frame();
-
 			viewport.swap_buffers();
 			return should_close;
 		}
@@ -68,9 +43,7 @@ int main(int, char**)
 		}
 	};
 
-	terraformer::ui::widgets::toolkit_instance toolkit{mainwin.handle()};
-
 	my_event_handler eh;
 	mainwin.set_event_handler(std::ref(eh));
-	gui_ctxt.wait_events(std::ref(eh), std::ref(mainwin), std::ref(toolkit));
+	gui_ctxt.wait_events(std::ref(eh), std::ref(mainwin));
 }
