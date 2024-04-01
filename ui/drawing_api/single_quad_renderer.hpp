a77cde973627a28ba3965a7d5720d7b5b5861150
Torbjörn Rathsman <mialsudril@gmail.com>
2024-04-01 09:35:55 +0200

    Introduce parameters to render

--- ui/drawing_api/single_quad_renderer.hpp
+++ ui/drawing_api/single_quad_renderer.hpp
@@ -9,36 +9,43 @@ namespace terraformer::ui::drawing_api
 	class single_quad_renderer
 	{
 	public:
-		void render()
+		void render(
+			location where = location{0.0f, 0.0f, 0.0f},
+			scaling scale = scaling{1.0f, 1.0f, 1.0f}
+		)
 		{
 			m_program.bind();
 			m_mesh.bind();
+			glUniform4f(0, where[0], where[1], where[2], 1.0f);
+			glUniform4f(1, scale[0], scale[1], scale[2], 0.0f);
 			gl_bindings::draw_triangles();
 		}
 
 	private:
-		gl_mesh<unsigned int, terraformer::location> m_mesh{
+		gl_mesh<unsigned int, displacement> m_mesh{
 			std::array<unsigned int, 6>{
 				0, 1, 2, 0, 2, 3
 			},			
-			std::array<terraformer::location, 4>{
-				terraformer::location{-0.5f, -0.5f, 0.0f},
-				terraformer::location{0.5f, -0.5f, 0.0f},
-				terraformer::location{0.5f, 0.5f, 0.0f},
-				terraformer::location{-0.5f, 0.5f, 0.0f},
+			std::array<displacement, 4>{
+				displacement{-0.5f, -0.5f, 0.0f},
+				displacement{0.5f, -0.5f, 0.0f},
+				displacement{0.5f, 0.5f, 0.0f},
+				displacement{-0.5f, 0.5f, 0.0f},
 			}
 		};
 		
 		gl_program m_program{
 			gl_shader<GL_VERTEX_SHADER>{
 				R"(#version 460 core
-layout (location = 0) in vec4 input_location;
+layout (location = 0) in vec4 input_offset;
+layout (location = 0) uniform vec4 location;
+layout (location = 1) uniform vec4 scale;
 
 out vec4 vertex_color;
 
 void main()
 {
-	gl_Position = input_location;
+	gl_Position = location + scale*input_offset;
 	vertex_color = vec4(0.5, 0.5, 0.5, 1.0);
 })"
 			},
