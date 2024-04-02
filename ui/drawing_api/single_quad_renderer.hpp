900520cb705e3d7a254d93131d9147f5b994e088
Torbjörn Rathsman <milasudril@gmail.com>
2024-04-02 21:38:15 +0200

    Work on coordinate transforms

--- ui/drawing_api/single_quad_renderer.hpp
+++ ui/drawing_api/single_quad_renderer.hpp
@@ -10,14 +10,14 @@ namespace terraformer::ui::drawing_api
 	class single_quad_renderer
 	{
 	public:
-		void set_world_transform(location origin, wsapi::fb_size size)
+		void set_world_transform(location where, wsapi::fb_size size)
 		{
 			scaling const s{1.0f/static_cast<float>(size.width), 1.0f/static_cast<float>(size.height), 1.0f};
 			m_program.bind();
-			glUniform4f(3, origin[0], origin[1], origin[2], 1.0f);
+			glUniform4f(3, where[0], where[1], where[2], 1.0f);
 			glUniform4f(4, s[0], s[1], s[2], 0.0f);
 		}
-		
+
 		void render(location where, location origin, scaling scale)
 		{
 			m_program.bind();
@@ -32,7 +32,7 @@ namespace terraformer::ui::drawing_api
 		gl_mesh<unsigned int, location> m_mesh{
 			std::array<unsigned int, 6>{
 				0, 1, 2, 0, 2, 3
-			},			
+			},
 			std::array<location, 4>{
 				location{-1.0f, -1.0f, 0.0f},
 				location{1.0f, -1.0f, 0.0f},
@@ -40,25 +40,26 @@ namespace terraformer::ui::drawing_api
 				location{-1.0f,1.0f, 0.0f},
 			}
 		};
-		
+
 		gl_program m_program{
 			gl_shader<GL_VERTEX_SHADER>{
 				R"(#version 460 core
 layout (location = 0) in vec4 input_offset;
 
-layout (location = 0) uniform vec4 location;
+layout (location = 0) uniform vec4 model_location;
 layout (location = 1) uniform vec4 model_origin;
 layout (location = 2) uniform vec4 model_size;
-layout (location = 3) uniform vec4 world_origin;
+layout (location = 3) uniform vec4 world_location;
 layout (location = 4) uniform vec4 world_scale;
 
 out vec4 vertex_color;
 
 void main()
 {
-	gl_Position =
-		 world_origin
-		+model_size*world_scale*(input_offset - model_origin);
+	const vec4 world_origin = vec4(0.0, 0.0, 0.0, 1.0);
+	vec4 loc = model_location + model_size*(input_offset - model_origin);
+	gl_Position = world_location + world_scale*(loc - world_origin);
+
 	vertex_color = vec4(0.5, 0.5, 0.5, 1.0);
 })"
 			},
