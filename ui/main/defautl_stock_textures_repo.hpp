c2a8b6ebbeea435a1daf3cf909f79099d5d7d96c
Torbjörn Rathsman <milasudril@gmail.com>
2024-04-07 13:32:13 +0200

    Introduce default stock textures repo

---
+++ ui/main/defautl_stock_textures_repo.hpp
@@ -0,0 +1,13 @@
+#ifndef DEFAULT_STOCK_TEXTUREs_REPO_HPP
+#define DEFAULT_STOCK_TEXTUREs_REPO_HPP
+
+namespace terraformer::ui::main
+{
+	class defautl_stock_textures_repo
+	{
+	public:
+	private:
+	};
+};
+
+#endif
