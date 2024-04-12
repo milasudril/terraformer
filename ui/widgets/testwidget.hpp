2665392c7d4e33c7a9702f5146225b31a384f32d
Torbjörn Rathsman <milasudril@gmail.com>
2024-04-12 21:09:12 +0200

    Add some state to testwidget

--- ui/widgets/testwidget.hpp
+++ ui/widgets/testwidget.hpp
@@ -2,6 +2,7 @@
 #define TERRAFORMER_UI_WIDGETS_TESTWIDGET_HPP
 
 #include "ui/drawing_api/gl_texture.hpp"
+#include "ui/theming/color_scheme.hpp"
 
 namespace terraformer::ui::widgets
 {
@@ -9,7 +10,13 @@ namespace terraformer::ui::widgets
 	{
 	public:
 		void render()
-		{ }
+		{
+			if(m_dirty)
+			{
+				printf("Rendering\n");
+				m_dirty = false;
+			}
+		}
 
 		auto const& background() const
 		{ return m_background; }
@@ -20,15 +27,29 @@ namespace terraformer::ui::widgets
 		bool handle_event(wsapi::cursor_position)
 		{ return false;}
 
-		bool handle_event(wsapi::mouse_button_event const&)
-		{ return false; }
+		bool handle_event(wsapi::mouse_button_event const& mbe)
+		{
+			auto const num_colors = std::size(theming::current_color_scheme.misc_dark_colors);
+
+			if((mbe.button == 0 || mbe.button == 1)
+				&& mbe.action == wsapi::button_action::release)
+			{
+				auto const dir = mbe.button == 0 ? -1 : 1;
+				m_current_color = (m_current_color + dir + num_colors)%num_colors;
+				m_dirty = true;
+				printf("Hej %zu\n", m_current_color);
+			}
+			return false;
+		}
 
 		wsapi::fb_size handle_event(wsapi::fb_size size)
-		{ return size; }
+		{	return size; }
 
 	private:
 		drawing_api::gl_texture m_foreground;
 		drawing_api::gl_texture m_background;
+		size_t m_current_color = 0;
+		bool m_dirty = false;
 	};
 }
 
