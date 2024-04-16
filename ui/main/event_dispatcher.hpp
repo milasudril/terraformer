12986ccc1eb17968f43af32d87d12767488d079c
Torbjörn Rathsman <milasudril@gmail.com>
2024-04-16 20:19:43 +0200

    Use value_of from event_dispatcher

--- ui/main/event_dispatcher.hpp
+++ ui/main/event_dispatcher.hpp
@@ -3,6 +3,7 @@
 
 #include "ui/wsapi/events.hpp"
 #include "ui/drawing_api/single_quad_renderer.hpp"
+#include "lib/common/value_accessor.hpp"
 
 namespace terraformer::ui::main
 {
@@ -12,10 +13,10 @@ namespace terraformer::ui::main
 	public:
 		template<class T1, class T2, class T3, class T4>
 		requires(
-				 std::is_same_v<std::decay_t<T1>, WidgetContainer>
-			&& std::is_same_v<std::decay_t<T2>, WindowController>
-			&& std::is_same_v<std::decay_t<T3>, Renderer>
-			&& std::is_same_v<std::decay_t<T4>, ErrorHandler>
+				 std::is_same_v<std::remove_cvref_t<T1>, WidgetContainer>
+			&& std::is_same_v<std::remove_cvref_t<T2>, WindowController>
+			&& std::is_same_v<std::remove_cvref_t<T3>, Renderer>
+			&& std::is_same_v<std::remove_cvref_t<T4>, ErrorHandler>
 		)
 		explicit event_dispatcher(T1&& widget_container, T2&& window_controller, T3&& renderer, T4&& error_handler):
 			m_widget_container{std::forward<T1>(widget_container)},
@@ -26,64 +27,69 @@ namespace terraformer::ui::main
 
 		template<auto WindowId>
 		void error_detected(wsapi::error_message const& msg) noexcept
-		{ m_error_handler.template error_detected<WindowId>(msg); }
+		{ value_of(m_error_handler).template error_detected<WindowId>(msg); }
 
 		template<auto WindowId>
 		void handle_mouse_button_event(wsapi::mouse_button_event const& event)
-		{ m_widget_container.template handle_event<WindowId>(event); }
+		{ value_of(m_widget_container).template handle_event<WindowId>(event); }
 
 		template<auto WindowId>
 		void handle_cursor_motion_event(wsapi::cursor_motion_event const& event)
-		{ m_widget_container.template handle_event<WindowId>(event); }
+		{ value_of(m_widget_container).template handle_event<WindowId>(event); }
 
 		template<auto WindowId>
 		void window_is_closing()
-		{ m_window_controller.template window_is_closing<WindowId>(); }
+		{ value_of(m_window_controller).template window_is_closing<WindowId>(); }
 
 		template<auto WindowId>
 		void handle_cursor_enter_leave_event(terraformer::ui::wsapi::cursor_enter_leave_event const& event)
-		{ m_window_controller.template cursor_at_window_boundary<WindowId>(event); }
+		{ value_of(m_window_controller).template cursor_at_window_boundary<WindowId>(event); }
 
 		template<auto WindowId>
 		void framebuffer_size_changed(terraformer::ui::wsapi::fb_size size)
 		{
 			m_fb_size = size;
-			m_renderer
+			value_of(m_renderer)
 				.set_viewport(0, 0, size.width, size.height)
 				.set_world_transform(terraformer::location{-1.0f, 1.0f, 0.0f}, size);
-			m_widget_container.template handle_event<WindowId>(size);
+			value_of(m_widget_container).template handle_event<WindowId>(size);
 		}
 
 		template<class Viewport>
 		bool operator()(Viewport&& viewport)
 		{
-			m_renderer.clear_buffers();
+			value_of(m_renderer).clear_buffers();
 			render();
-			viewport.swap_buffers();
-			return m_window_controller.main_loop_should_exit(viewport);
+			value_of(viewport).swap_buffers();
+			return value_of(m_window_controller).main_loop_should_exit(viewport);
 		}
 
 		void render()
 		{
-			m_widget_container.render();
-			m_renderer.render(
+			value_of(m_widget_container).render();
+			value_of(m_renderer).render(
 				terraformer::location{0.0f, 0.0f, 0.0f},
 				terraformer::location{-1.0f, 1.0f, 0.0f},
 				terraformer::scaling{static_cast<float>(m_fb_size.width), static_cast<float>(m_fb_size.height), 1.0f},
-				m_widget_container.background(),
-				m_widget_container.foreground()
+				value_of(m_widget_container).background(),
+				value_of(m_widget_container).foreground()
 			);
 
-			m_widget_container.show_widgets(m_renderer);
+			value_of(m_widget_container).show_widgets(value_of(m_renderer));
 		}
 
 	private:
-		wsapi::fb_size m_fb_size;
+		wsapi::fb_size m_fb_size{};
 		WidgetContainer m_widget_container;
 		WindowController m_window_controller;
 		Renderer m_renderer;
 		ErrorHandler m_error_handler;
 	};
+
+	template<class T1, class T2, class T3, class T4>
+	event_dispatcher(T1&&, T2&&, T3&&, T4&&) ->
+	event_dispatcher<std::remove_cvref_t<T1>, std::remove_cvref_t<T2>, std::remove_cvref_t<T3>, std::remove_cvref_t<T4>>;
 }
+
 
 #endif
