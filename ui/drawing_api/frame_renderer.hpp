0f601cb35125b42af4cc277b87b5acadc71a6828
Torbjörn Rathsman <milasudril@gmail.com>
2024-05-04 20:31:18 +0200

    Make adjacent vertex at the same index

--- ui/drawing_api/frame_renderer.hpp
+++ ui/drawing_api/frame_renderer.hpp
@@ -76,7 +76,7 @@ namespace terraformer::ui::drawing_api
 		gl_mesh<unsigned int> m_mesh{
 			std::array<unsigned int, 12>{
 				0, 1, 2, 2, 1, 3,
-				4, 5, 6, 6, 5, 7
+				4, 6, 5, 5, 6, 7
 			}
 		};
 
@@ -110,10 +110,10 @@ const vec4 coords[8] = vec4[8](
 	vec4(0.5f, 0.5f, 0.0f, 1.0f),
 
 	// Left
-	vec4(-0.5f, -0.5f, 0.0f, 1.0f),
-	vec4(-0.5f, -0.5f, 0.0f, 1.0f),
 	vec4(-0.5f, 0.5f, 0.0f, 1.0f),
-	vec4(-0.5f, 0.5f, 0.0f, 1.0f)
+	vec4(-0.5f, 0.5f, 0.0f, 1.0f),
+	vec4(-0.5f, -0.5f, 0.0f, 1.0f),
+	vec4(-0.5f, -0.5f, 0.0f, 1.0f)
 );
 
 const vec4 offsets[8] = vec4[8](
@@ -125,9 +125,9 @@ const vec4 offsets[8] = vec4[8](
 
 	// Left
 	vec4(0.0f, 0.0f, 0.0f, 0.0f),
-	vec4(1.0f, 1.0f, 0.0f, 0.0f),
+	vec4(1.0f, -1.0f, 0.0f, 0.0f),
 	vec4(0.0f, 0.0f, 0.0f, 0.0f),
-	vec4(1.0f, -1.0f, 0.0f, 0.0f)
+	vec4(1.0f, 1.0f, 0.0f, 0.0f)
 );
 
 void main()
