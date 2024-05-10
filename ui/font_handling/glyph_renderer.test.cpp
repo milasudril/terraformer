e976ba172508269236b17a1e7ee9c13a7aa3146e
Torbjörn Rathsman <milasudril@gmail.com>
2024-05-10 19:28:48 +0200

    Dump glyph to terminal in unit test

--- ui/font_handling/glyph_renderer.test.cpp
+++ ui/font_handling/glyph_renderer.test.cpp
@@ -5,6 +5,25 @@
 #include "./font_mapper.hpp"
 #include <testfwk/testfwk.hpp>
 
+namespace
+{
+	void dump_glyph(terraformer::ui::font_handling::glyph const& g)
+	{
+		for(uint32_t y = 0; y != g.image.height(); ++y)
+		{
+			for(uint32_t x = 0; x != g.image.width(); ++x)
+			{
+				if(g.image(x, y) > 128)
+				{ putchar('*'); }
+				else
+				{ putchar(' '); }
+			}
+			putchar('\n');
+		}
+		putchar('\n');
+	}
+}
+
 TESTCASE(terraformer_ui_font_handling_glyph_renderer_load_latin1)
 {
 	terraformer::ui::font_handling::font_mapper fonts;
@@ -14,15 +33,18 @@ TESTCASE(terraformer_ui_font_handling_glyph_renderer_load_latin1)
 
 	// Load in cold state
 	auto& glyph_1 = renderer.get_glyph(24, terraformer::ui::font_handling::codepoint{65});
+	dump_glyph(glyph_1);
 
 	// Load with same size as before
 	auto& glyph_2 = renderer.get_glyph(24, terraformer::ui::font_handling::codepoint{66});
 	EXPECT_NE(&glyph_1, &glyph_2);
+	dump_glyph(glyph_2);
 
 	// Load with different size as before
 	auto& glyph_3 = renderer.get_glyph(16, terraformer::ui::font_handling::codepoint{66});
 	EXPECT_NE(&glyph_1, &glyph_3);
 	EXPECT_NE(&glyph_2, &glyph_3);
+	dump_glyph(glyph_3);
 
 	// Fetch all loaded glyphs
 	auto& glyph_4 = renderer.get_glyph(24, terraformer::ui::font_handling::codepoint{65});
