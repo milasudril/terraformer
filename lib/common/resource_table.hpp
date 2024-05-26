3612836557027461e0d7e378515159443906f235
Torbjörn Rathsman <milasudril@gmail.com>
2024-05-26 17:25:23 +0200

    Complete resource_table

--- lib/common/resource_table.hpp
+++ lib/common/resource_table.hpp
@@ -3,6 +3,7 @@
 
 #include <unordered_map>
 #include <memory>
+#include <cassert>
 
 namespace terraformer
 {
@@ -14,10 +15,6 @@ namespace terraformer
 	public:
 		using base::base;
 		using base::contains;
-		using base::insert;
-		using base::emplace;
-		using base::try_emplace;
-		using base::insert_or_assign;
 
 		template<class KeyType>
 		std::shared_ptr<Value const> at_ptr(KeyType&& key) const
@@ -33,16 +30,42 @@ namespace terraformer
 			return i != base::end()? i->second : std::shared_ptr<Value>{};
 		}
 
+		template<class KeyType, class ... Args>
+		std::pair<std::shared_ptr<Value>, bool> emplace(KeyType&& key, Args&&... args)
+		{
+			auto const i = base::emplace(std::forward<Key>(key), std::make_shared<Value>(args...));
+			return std::pair{i.first->second, i.second};
+		}
+
+		template<class KeyType, class... Args>
+		std::pair<std::shared_ptr<Value>, bool> insert_or_assign(KeyType&& key, Args&&... args)
+		{
+			auto const i = base::insert_or_assign(std::forward<KeyType>(key), std::make_shared<Value>(args...));
+			return std::pair{i.first->second, i.second};
+		}
+
+		template<class KeyType, class... Args>
+		std::pair<std::shared_ptr<Value>, bool> insert_or_assign_linked(KeyType&& key, Args&&... args)
+		{
+			auto const i = base::find(key);
+			if(i == base::end())
+			{ return emplace(std::forward<Key>(key), std::forward<Args>(args)...); }
+
+			*i->second = Value{std::forward<Args>(args)...};
+			return std::pair{i->second, false};
+		}
+
+
 		template<class KeyType1, class KeyType2>
 		std::pair<std::shared_ptr<Value>, bool> create_or_replace_link(KeyType1&& old_key, KeyType2&& new_key)
 		{
 			auto const i_old = base::find(old_key);
 			if(i_old == base::end())
-			{ return std::pair<Value*, bool>{nullptr, false}; }
+			{ return std::pair<std::shared_ptr<Value>, bool>{nullptr, false}; }
 
 			auto const i = base::insert_or_assign(
 				std::forward<KeyType2>(new_key),
-				i->second
+				i_old->second
 			);
 
 			return std::pair{i_old->second, i.second};
@@ -51,13 +74,16 @@ namespace terraformer
 		template<class KeyType1, class KeyType2>
 		std::pair<std::shared_ptr<Value>, bool> create_link(KeyType1&& old_key, KeyType2&& new_key)
 		{
+			assert(old_key != new_key);
 			auto const i_old = base::find(old_key);
 			if(i_old == base::end())
 			{ return std::pair<std::shared_ptr<Value>, bool>{nullptr, false}; }
 
 			auto const i = base::insert(
-				std::forward<KeyType2>(new_key),
-				i->second
+				typename base::value_type{
+					std::forward<KeyType2>(new_key),
+					i_old->second
+				}
 			);
 
 			return std::pair{i_old->second, i.second};
--- lib/common/resource_table.test.cpp
+++ lib/common/resource_table.test.cpp
@@ -3,3 +3,83 @@
 #include "./resource_table.hpp"
 
 #include <testfwk/testfwk.hpp>
+
+#include <string>
+
+TESTCASE(terraformer_resource_table_default_state)
+{
+	terraformer::resource_table<std::string, int> table;
+	EXPECT_EQ(table.contains("Hej"), false);
+	EXPECT_EQ(table.at_ptr("Hej"), nullptr);
+	EXPECT_EQ(std::as_const(table).contains("Hej"), false);
+	EXPECT_EQ(std::as_const(table).at_ptr("Hej"), nullptr);
+
+	auto const link = table.create_link("Hej", "New key");
+	EXPECT_EQ(link.first, nullptr);
+	EXPECT_EQ(link.second, false);
+}
+
+TESTCASE(terraformer_resource_table_emplace)
+{
+	terraformer::resource_table<std::string, int> table;
+	REQUIRE_EQ(std::as_const(table).contains("Hej"), false);
+	REQUIRE_EQ(std::as_const(table).at_ptr("Hej"), nullptr);
+
+	auto const res1 = table.emplace("Hej", 123);
+
+	EXPECT_EQ(res1.second, true);
+	REQUIRE_NE(res1.first, nullptr);
+	EXPECT_EQ(*res1.first, 123);
+	REQUIRE_EQ(std::as_const(table).contains("Hej"), true);
+	EXPECT_EQ(std::as_const(table).at_ptr("Hej"), res1.first);
+
+	auto const res2 = table.emplace("Hej", 456);
+	EXPECT_EQ(res2.first, res1.first);
+	EXPECT_EQ(res2.second, false);
+
+	auto const link = table.create_link("Hej", "New key");
+	EXPECT_EQ(link.first, res1.first);
+	EXPECT_EQ(link.second, true);
+
+	auto const link2 = table.create_link("Hej", "New key");
+	EXPECT_EQ(link2.first, link.first);
+	EXPECT_EQ(link2.second, false);
+
+	auto const res3 = table.emplace("New key", 456);
+	EXPECT_EQ(res3.first, res1.first);
+
+	auto const res4 = table.insert_or_assign("Hej", 456);
+	REQUIRE_NE(res4.first, nullptr);
+	EXPECT_EQ(res4.second, false);
+	EXPECT_EQ(*res4.first, 456);
+	EXPECT_EQ(*res3.first, 123);
+
+	auto const link3 = table.create_or_replace_link("New key", "Hej");
+	REQUIRE_NE(link3.first, nullptr);
+	EXPECT_EQ(*link3.first, 123);
+	EXPECT_EQ(link3.second, false);
+
+	auto const link4 = table.create_or_replace_link("Hej", "Foobar");
+	REQUIRE_NE(link4.first, nullptr);
+	EXPECT_EQ(*link4.first, 123);
+	EXPECT_EQ(link4.second, true);
+
+	auto const res5 = table.insert_or_assign_linked("Foobar", 789);
+	REQUIRE_NE(res5.first, nullptr);
+	EXPECT_EQ(*res5.first, 789);
+	EXPECT_EQ(res5.second, false);
+	EXPECT_EQ(*link4.first, 789);
+	EXPECT_EQ(*link3.first, 789);
+
+	auto const res6 = table.insert_or_assign_linked("Kaka", 23243);
+	REQUIRE_NE(res6.first, nullptr);
+	EXPECT_EQ(*res6.first, 23243);
+	EXPECT_EQ(res6.second, true);
+	EXPECT_EQ(*link4.first, 789);
+	EXPECT_EQ(*link3.first, 789);
+
+	auto const link5 = table.create_or_replace_link("Non-existing-key", "Other key");
+	EXPECT_EQ(link5.first, nullptr);
+	EXPECT_EQ(link5.second, false);
+	EXPECT_EQ(table.contains("Other key"), false);
+}
