1872c6d32468943321e5dee82b558443aadce539
Torbjörn Rathsman <milasudril@gmail.com>
2024-06-01 19:40:19 +0200

    Test append

--- lib/common/object_tree.hpp
+++ lib/common/object_tree.hpp
@@ -23,21 +23,54 @@ namespace terraformer
 	class object_tree
 	{
 	public:
+		using map_type = std::unordered_map<std::string, shared_any, string_hash, std::equal_to<>>;
+		using array_type = single_array<shared_any>;
+
+		template<class T>
+		static constexpr auto is_leaf_type_v = !(
+			  std::is_same_v<std::remove_cvref_t<T>, array_type>
+			||std::is_same_v<std::remove_cvref_t<T>, map_type>
+		);
+
+		object_tree(object_tree const&) = delete;
+		object_tree& operator=(object_tree const&) = delete;
+		object_tree(object_tree&&) = default;
+		object_tree& operator=(object_tree&&) = default;
+
 		explicit object_tree() = default;
 
 		explicit object_tree(shared_any&& obj): m_value{std::move(obj)}{}
 
 		explicit object_tree(shared_any const& obj):m_value{obj}{}
 
-		using map_type = std::unordered_map<std::string, shared_any, string_hash, std::equal_to<>>;
-		using array_type = single_array<shared_any>;
-		using index_type = array_type::index_type;
+		template<class TypeOfValueToInsert, class ... Args>
+		requires(is_leaf_type_v<TypeOfValueToInsert>)
+		object_tree append(Args&&... args)
+		{
+			if(!m_value)
+			{ m_value = shared_any{std::type_identity<array_type>{}}; }
+
+			if(auto const array = m_value.template get_if<array_type>(); array != nullptr)
+			{
+				array->push_back(shared_any{std::type_identity<TypeOfValueToInsert>{}, std::forward<Args>(args)...});
+				return object_tree{m_value};
+			}
+
+			return object_tree{};
+		}
+
+
+		bool is_null() const
+		{ return !m_value; }
+
+		//FIXME: Add `const` support
 
 		template<class T>
+		requires(is_leaf_type_v<T>)
 		operator T*()
 		{ return m_value.template get_if<T>(); }
 
-		auto operator/(std::string_view key)
+		object_tree operator/(std::string_view key)
 		{
 			auto const val = m_value.template get_if<map_type>();
 			if(val == nullptr)
@@ -47,13 +80,15 @@ namespace terraformer
 			return i != std::end(*val)? object_tree{i->second} : object_tree{};
 		}
 
-		auto operator/(index_type index)
+		object_tree operator/(size_t index)
 		{
+			using index_type = array_type::index_type;
+
 			auto const val = m_value.template get_if<array_type>();
 			if(val == nullptr)
 			{ return object_tree{}; }
 
-			return index < std::size(*val)? object_tree{(*val)[index]} : object_tree{};
+			return index < std::size(*val).get()? object_tree{(*val)[index_type{index}]} : object_tree{};
 		}
 
 		template<class Func>
@@ -74,6 +109,17 @@ namespace terraformer
 			}
 			else
 			{ f(object_tree{m_value}); }
+		}
+
+		size_t non_recursive_size() const
+		{
+			if(auto const map = m_value.template get_if<map_type>(); map != nullptr)
+			{ return std::size(*map); }
+			else
+			if(auto const array = m_value.template get_if<array_type>(); array != nullptr)
+			{ return std::size(*array).get(); }
+			else
+			{ return m_value? static_cast<size_t>(1) : 0; }
 		}
 
 	private:
--- lib/common/object_tree.test.cpp
+++ lib/common/object_tree.test.cpp
@@ -3,3 +3,36 @@
 #include "./object_tree.hpp"
 
 #include <testfwk/testfwk.hpp>
+
+TESTCASE(terraformer_object_tree_append)
+{
+	terraformer::object_tree tree;
+	EXPECT_EQ(tree.is_null(), true);
+
+	tree.append<int>(1)
+		.append<double>(2.5)
+		.append<std::string>("Hello, World");
+
+	REQUIRE_EQ(tree.non_recursive_size(), 3);
+
+	{
+		int const* val = tree/0;
+		REQUIRE_NE(val, nullptr);
+		EXPECT_EQ(*val, 1);
+	}
+
+	{
+		double const* val = tree/1;
+		REQUIRE_NE(val, nullptr);
+		EXPECT_EQ(*val, 2.5);
+	}
+
+	{
+		std::string const* val = tree/2;
+		REQUIRE_NE(val, nullptr);
+		EXPECT_EQ(*val, "Hello, World");
+	}
+
+	EXPECT_EQ((tree/3).is_null(), true);
+	EXPECT_EQ(static_cast<int*>(tree/2), nullptr);
+}
