f0f839ade614ae0b00eb63cc530488718264876b
Torbjörn Rathsman <milasudril@gmail.com>
2024-06-15 09:25:36 +0200

    Move holder outside of shared_any

--- lib/common/shared_any.hpp
+++ lib/common/shared_any.hpp
@@ -11,9 +11,60 @@
 
 namespace terraformer
 {
+	struct shared_any_holder
+	{
+		static void noop(void*){}
+
+		shared_any_holder() = default;
+		template<class T, class ... Args>
+		explicit shared_any_holder(std::type_identity<T>, Args&&... args):
+			pointer{new T(std::forward<Args>(args)...)},
+			current_type{std::type_index{typeid(T)}},
+			usecount{new size_t(1)},
+			destroy{[](void* obj){ delete static_cast<T*>(obj);}}
+		{}
+
+		std::strong_ordering operator<=>(shared_any_holder const& other) const noexcept
+		{ return std::compare_three_way{}(pointer, other.pointer); }
+
+		void inc_usecount() noexcept
+		{ ++(*usecount); }
+
+		void dec_usecount() noexcept
+		{
+			if(usecount != nullptr)
+			{
+				--(*usecount);
+				if(*usecount == 0)
+				{
+					destroy(pointer);
+					delete usecount;
+				}
+			}
+		}
+
+		template<class T>
+		T* get_if() const noexcept
+		{
+			if(current_type == std::type_index{typeid(T)})
+			{ return static_cast<T*>(pointer); }
+			return nullptr;
+		}
+
+		size_t use_count() const noexcept
+		{ return usecount != nullptr? *usecount: static_cast<size_t>(0); }
+
+		void* pointer = nullptr;
+		std::type_index current_type = std::type_index{typeid(void)};
+		size_t* usecount = nullptr;
+		void (*destroy)(void*) = noop;
+	};
+
 	class shared_any
 	{
 	public:
+		using holder = shared_any_holder;
+
 		shared_any() noexcept = default;
 
 		template<class T, class ... Args>
@@ -76,55 +127,6 @@ namespace terraformer
 		{ return std::bit_cast<intptr_t>(m_holder.pointer); }
 
 	private:
-		static void noop(void*){}
-
-		struct holder
-		{
-			holder() = default;
-			template<class T, class ... Args>
-			explicit holder(std::type_identity<T>, Args&&... args):
-				pointer{new T(std::forward<Args>(args)...)},
-				current_type{std::type_index{typeid(T)}},
-				usecount{new size_t(1)},
-				destroy{[](void* obj){ delete static_cast<T*>(obj);}}
-			{}
-
-			std::strong_ordering operator<=>(holder const& other) const noexcept
-			{ return std::compare_three_way{}(pointer, other.pointer); }
-
-			void inc_usecount() noexcept
-			{ ++(*usecount); }
-
-			void dec_usecount() noexcept
-			{
-				if(usecount != nullptr)
-				{
-					--(*usecount);
-					if(*usecount == 0)
-					{
-						destroy(pointer);
-						delete usecount;
-					}
-				}
-			}
-
-			template<class T>
-			T* get_if() const noexcept
-			{
-				if(current_type == std::type_index{typeid(T)})
-				{ return static_cast<T*>(pointer); }
-				return nullptr;
-			}
-
-			size_t use_count() const noexcept
-			{ return usecount != nullptr? *usecount: static_cast<size_t>(0); }
-
-			void* pointer = nullptr;
-			std::type_index current_type = std::type_index{typeid(void)};
-			size_t* usecount = nullptr;
-			void (*destroy)(void*) = noop;
-		};
-
 		holder m_holder;
 	};
 }
