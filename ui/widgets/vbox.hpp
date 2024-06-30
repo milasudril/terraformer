3a1c956010d4948eef3cc75387a66cfe2978c1ea
Torbjörn Rathsman <milasudril@gmail.com>
2024-06-30 15:18:09 +0200

    Update widget sizes when layout is being updated

--- ui/widgets/vbox.hpp
+++ ui/widgets/vbox.hpp
@@ -124,26 +124,6 @@ namespace terraformer::ui::widgets
 
 		wsapi::fb_size handle_event(wsapi::fb_size size)
 		{
-			// TODO: The available size should not include any decorations (frames, scrollbars etc)
-			auto const size_callbacks = m_widgets.size_callbacks();
-			auto const widget_pointers = m_widgets.widget_pointers();
-			auto const widget_visibilities = m_widgets.widget_visibilities();
-			auto const widget_geometries = m_widgets.widget_geometries();
-			auto const n = std::size(m_widgets);
-			for(auto k = m_widgets.first_element_index(); k != n; ++k)
-			{
-				// TODO: Maybe size needs to be propagated to invisible widgets
-				if(widget_visibilities[k] == main::widget_visibility::visible) [[likely]]
-				{
-					size_callbacks[k](
-						widget_pointers[k],
-						wsapi::fb_size {
-							.width = static_cast<int>(widget_geometries[k].size[0]),
-							.height = static_cast<int>(widget_geometries[k].size[1])
-						}
-					);
-				}
-			}
 			return size;
 		}
 
@@ -163,6 +143,7 @@ namespace terraformer::ui::widgets
 			auto const widget_geometries = m_widgets.widget_geometries();
 			auto const size_constraints_callbacks = m_widgets.size_constraints_callbacks();
 			auto const widget_visibilities = m_widgets.widget_visibilities();
+			auto const size_callbacks = m_widgets.size_callbacks();
 			auto const n = std::size(m_widgets);
 			auto min_width = 0.0f;
 			auto max_width = std::numeric_limits<float>::infinity();
@@ -182,6 +163,14 @@ namespace terraformer::ui::widgets
 					min_width = std::max(min_width, widget_geometries[k].size[0]);
 					max_width = std::min(max_width, constraints.width.max);
 					height += widget_geometries[k].size[1] + 4.0f;
+
+					size_callbacks[k](
+						widget_pointers[k],
+						wsapi::fb_size {
+							.width = static_cast<int>(widget_geometries[k].size[0]),
+							.height = static_cast<int>(widget_geometries[k].size[1])
+						}
+					);
 				}
 			}
 
