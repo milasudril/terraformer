c1e21b2cf1bdb9785ba9e7b99b24295985337bad
Torbjörn Rathsman <milasudril@gmail.com>
2024-09-27 20:58:51 +0200

    Remove legacy theme loader

--- ui/theming/theme_loader.hpp
+++ ui/theming/theme_loader.hpp
@@ -7,7 +7,6 @@
 #include "ui/main/generic_texture.hpp"
 #include "ui/font_handling/font_mapper.hpp"
 #include "ui/font_handling/text_shaper.hpp"
-#include "lib/common/object_tree.hpp"
 
 namespace terraformer::ui::theming
 {
@@ -88,64 +87,6 @@ namespace terraformer::ui::theming
 				.test_pattern = test_pattern
 			}
 		};
-	}
-
-	template<class TextureType>
-	object_dict load_default_resources()
-	{
-		shared_any noisy_texture{
-			std::type_identity<main::generic_unique_texture>{},
-			std::type_identity<TextureType>{},
-			generate_noisy_texture<TextureType>()
-		};
-
-		shared_any white_texture{
-			std::type_identity<main::generic_unique_texture>{},
-			std::type_identity<TextureType>{},
-			generate_white_texture<TextureType>()
-		};
-
-		font_handling::font_mapper fonts;
- 		auto const fontfile = fonts.get_path("sans-serif");
-
-		shared_any body_text{
-			std::type_identity<font_handling::font>{},
-			std::move(font_handling::font{fontfile.c_str()}.set_font_size(11))
-		};
-
-		object_dict resources;
-		resources.insert<object_dict>(
-			"ui", std::move(object_dict{}
-				.insert<object_array>("panels", std::move(object_array{}
-					.append<object_dict>(std::move(object_dict{}
-						.insert_link("background_texture", noisy_texture)
-						.insert<rgba_pixel>("background_tint", default_color_scheme.main_panel.background)
-						.insert<object_dict>("margins", std::move(object_dict{}
-							.insert<float>("x", 4.0f)
-							.insert<float>("y", 4.0f))
-						))
-					))
-				)
-				.insert<object_dict>("command_area", std::move(object_dict{}
-					.insert<rgba_pixel>("background_tint", default_color_scheme.command_area.background)
-					.insert<rgba_pixel>("text_color", default_color_scheme.command_area.foreground)
-					.insert_link("font", body_text)
-					.insert<float>("background_intensity", 1.0f))
-				)
-				.insert<object_dict>("output_area", std::move(object_dict{}
-					.insert_link("font", body_text)
-					.insert<rgba_pixel>("text_color", default_color_scheme.output_area.foreground))
-				)
-				.insert<unsigned int>("widget_inner_margin", 4)
-				.insert<unsigned int>("3d_border_thickness", 2)
-				.insert<main::generic_unique_texture>(
-					"null_texture",
-					std::type_identity<TextureType>{},
-					generate_transparent_texture<TextureType>()
-				)
-			)
-		);
-		return resources;
 	}
 };
 
