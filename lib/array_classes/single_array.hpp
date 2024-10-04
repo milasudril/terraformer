60b80026058bd8e3cc7c3342325b1e93d4d3fccc
Torbjörn Rathsman <milasudril@gmail.com>
2024-10-04 18:56:43 +0200

    Make first_element_index static

--- lib/array_classes/single_array.hpp
+++ lib/array_classes/single_array.hpp
@@ -45,7 +45,7 @@ namespace terraformer
 		~single_array() noexcept
 		{ clear(); }
 
-		constexpr auto first_element_index() const noexcept
+		static constexpr auto first_element_index() noexcept
 		{ return index_type{}; }
 
 		constexpr auto last_element_index() const noexcept
