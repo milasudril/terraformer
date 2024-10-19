40de32ad7fd80ad0904c7b0f4ade763fbcc5f3d2
Torbjörn Rathsman <mialsudril@gmail.com>
2024-10-19 11:59:17 +0200

    Do not ignore collapsed widgets

--- ui/widgets/rowmajor_table_layout.cpp
+++ ui/widgets/rowmajor_table_layout.cpp
@@ -9,7 +9,6 @@ terraformer::ui::widgets::rowmajor_table_layout::	update_widget_locations(
 {
 	auto const sizes = std::as_const(widgets).sizes();
 	auto const widget_geometries = widgets.widget_geometries();
-	auto const widget_states = widgets.widget_states();
 
 	auto const n = std::size(widgets);
 	auto min_width = 0.0f;
@@ -18,20 +17,18 @@ terraformer::ui::widgets::rowmajor_table_layout::	update_widget_locations(
 
 	for(auto k = widgets.first_element_index(); k != n; ++k)
 	{
-		if(!widget_states[k].collapsed) [[likely]]
-		{
-			auto const& size = sizes[k];
-			widget_geometries[k].where = location{
-				margin_x,
-				-height,
-				0.0f
-			};
-			widget_geometries[k].origin = terraformer::location{-1.0f, 1.0f, 0.0f};
-			widget_geometries[k].size = size;
-			min_width = std::max(min_width, size[0]);
-			max_width = std::min(max_width, size[0]);
-			height += widget_geometries[k].size[1] + margin_y;
-		}
+		// NOTE: It is assumed that size is 0 if widget is collapsed
+		auto const& size = sizes[k];
+		widget_geometries[k].where = location{
+			margin_x,
+			-height,
+			0.0f
+		};
+		widget_geometries[k].origin = terraformer::location{-1.0f, 1.0f, 0.0f};
+		widget_geometries[k].size = size;
+		min_width = std::max(min_width, size[0]);
+		max_width = std::min(max_width, size[0]);
+		height += widget_geometries[k].size[1] + margin_y;
 	}
 
 	return scaling{
