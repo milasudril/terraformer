6bafe505a81847647665ce2bb92b22586538c9a6
Torbjörn Rathsman <milasudril@gmail.com>
2024-12-21 21:51:25 +0100

    Initial work on slider

---
+++ ui/widgets/slider.cpp
@@ -0,0 +1,98 @@
+//@	{"target":{"name":"slider.o"}}
+
+#include "./slider.hpp"
+
+#include "ui/drawing_api/image_generators.hpp"
+
+void terraformer::ui::widgets::slider::regenerate_textures()
+{
+	m_handle = generate(
+		drawing_api::beveled_rectangle{
+			.width = static_cast<uint32_t>(m_current_size.width),
+			.height = static_cast<uint32_t>(m_current_size.height),
+			.border_thickness = m_border_thickness,
+			.upper_left_color = m_bg_tint,
+			.lower_right_color = 0.25f*m_bg_tint + rgba_pixel{0.0f, 0.0f, 0.0f, 0.75f},
+			.fill_color = 0.5f*m_bg_tint + rgba_pixel{0.0f, 0.0f, 0.0f, 0.5f},
+		}
+	);
+
+	m_dirty_bits &= ~handle_dirty;
+
+	m_track = generate(
+		drawing_api::beveled_rectangle{
+			.width = static_cast<uint32_t>(m_current_size.width),
+			.height = static_cast<uint32_t>(m_current_size.height),
+			.border_thickness = m_border_thickness,
+			.upper_left_color = 0.25f*m_bg_tint + rgba_pixel{0.0f, 0.0f, 0.0f, 0.75f},
+			.lower_right_color = m_bg_tint,
+			.fill_color = 0.5f*m_bg_tint + rgba_pixel{0.0f, 0.0f, 0.0f, 0.5f},
+		}
+	);
+
+	m_dirty_bits &= ~track_dirty;
+}
+
+terraformer::ui::main::widget_layer_stack
+terraformer::ui::widgets::slider::prepare_for_presentation(main::graphics_backend_ref backend)
+{
+	if(m_dirty_bits & (handle_dirty|track_dirty)) [[unlikely]]
+	{ regenerate_textures(); }
+
+	auto const null_texture = m_null_texture->get_backend_resource(backend).get();
+
+	return main::widget_layer_stack{
+		.background = main::widget_layer{
+			.offset = displacement{},
+			.texture = m_track.get_backend_resource(backend).get(),
+			.tints = std::array{m_bg_tint, m_bg_tint, m_bg_tint, m_bg_tint}
+		},
+		.sel_bg_mask = main::widget_layer_mask{
+			.offset = displacement{},
+			.texture = null_texture
+		},
+		.selection_background = main::widget_layer{
+			.offset = displacement{},
+			.texture = null_texture,
+			.tints = std::array<rgba_pixel, 4>{}
+		},
+		.foreground = main::widget_layer{
+			.offset = displacement{},  // TODO: Derive offset from current value
+			.texture = m_handle.get_backend_resource(backend).get(),
+			.tints = std::array{m_fg_tint, m_fg_tint, m_fg_tint, m_fg_tint}
+		},
+		.frame = main::widget_layer{
+			.offset = displacement{},
+			.texture = null_texture,
+			.tints = std::array<rgba_pixel, 4>{}
+		},
+		// TODO: Use this layer for tick marks
+		.input_marker = main::widget_layer{
+			.offset = displacement{},
+			.texture = null_texture,
+			.tints = std::array<rgba_pixel, 4>{}
+		}
+	};
+}
+
+terraformer::scaling terraformer::ui::widgets::slider::compute_size(main::widget_width_request)
+{
+	// TODO: Express minimum size in terms of handle size
+	return scaling{2.0f*m_margin, 2.0f*m_margin, 1.0f};
+}
+
+terraformer::scaling terraformer::ui::widgets::slider::compute_size(main::widget_height_request)
+{
+return scaling{2.0f*m_margin, 2.0f*m_margin, 1.0f};
+}
+
+void terraformer::ui::widgets::slider::theme_updated(main::config const& cfg, main::widget_instance_info)
+{
+	m_margin = cfg.command_area.padding + cfg.command_area.border_thickness;
+	m_font = cfg.command_area.font;
+	m_bg_tint = cfg.command_area.colors.background;
+	m_fg_tint = cfg.command_area.colors.foreground;
+	m_border_thickness = static_cast<uint32_t>(cfg.command_area.border_thickness);
+	m_dirty_bits |= track_dirty | handle_dirty;
+	m_null_texture = cfg.misc_textures.null;
+}
---
+++ ui/widgets/slider.hpp
@@ -0,0 +1,123 @@
+//@	{"dependencies_extra":[{"ref":"./slider.o", "rel":"implementation"}]}
+
+#ifndef TERRAFORMER_UI_WIDGETS_SLIDER_HPP
+#define TERRAFORMER_UI_WIDGETS_SLIDER_HPP
+
+#include "ui/font_handling/text_shaper.hpp"
+#include "ui/main/widget.hpp"
+#include "ui/main/graphics_backend_ref.hpp"
+#include "lib/common/move_only_function.hpp"
+
+namespace terraformer::ui::widgets
+{
+	class slider:public main::widget_with_default_actions
+	{
+	public:
+		using widget_with_default_actions::handle_event;
+
+		enum class state{released, handle_grabbed};
+		enum class orientation{left_to_right, top_to_bottom, right_to_left, bottom_to_top};
+
+		slider& orientation(enum orientation new_orientation)
+		{
+			m_orientation = new_orientation;
+			m_dirty_bits |= handle_dirty|track_dirty;
+			return *this;
+		}
+
+		slider& value(bool new_value)
+		{
+			m_value = new_value;
+			m_state_current = new_value? state::handle_grabbed: state::released;
+			return *this;
+		}
+
+		bool value() const
+		{ return m_value; }
+
+		void regenerate_text_mask();
+
+		void regenerate_textures();
+
+		main::widget_layer_stack prepare_for_presentation(main::graphics_backend_ref backend);
+
+		void handle_event(main::cursor_leave_event const&, main::window_ref, main::ui_controller)
+		{ m_state_saved = std::exchange(m_state_current, state::released); }
+
+		void handle_event(main::cursor_enter_event const&, main::window_ref, main::ui_controller)
+		{ m_state_current = m_state_saved; }
+
+		void handle_event(main::mouse_button_event const& mbe, main::window_ref, main::ui_controller)
+		{
+			if(mbe.button == 0)
+			{
+				switch(mbe.action)
+				{
+					case main::mouse_button_action::press:
+						m_state_current = state::handle_grabbed;
+						break;
+
+					case main::mouse_button_action::release:
+						m_state_current = state::released;
+						m_state_saved = state::released;
+						break;
+				}
+			}
+		}
+
+		void handle_event(main::keyboard_button_event const&, main::window_ref, main::ui_controller)
+		{
+			// TODO
+		}
+
+		void handle_event(main::keyboard_focus_leave_event, main::window_ref, main::ui_controller)
+		{ m_state_saved = std::exchange(m_state_current, state::released); }
+
+		scaling compute_size(main::widget_width_request req);
+
+		scaling compute_size(main::widget_height_request req);
+
+		void handle_event(main::fb_size size)
+		{
+			m_current_size = size;
+			m_dirty_bits |= track_dirty;
+		}
+
+		void theme_updated(main::config const& cfg, main::widget_instance_info);
+
+		main::layout_policy_ref get_layout() const
+		{ return main::layout_policy_ref{}; }
+
+		main::widget_collection_ref get_children()
+		{ return main::widget_collection_ref{}; }
+
+		main::widget_collection_view get_children() const
+		{ return main::widget_collection_view{}; }
+
+	private:
+		main::widget_user_interaction_handler<slider> m_on_activated{no_operation_tag{}};
+		static constexpr unsigned int handle_dirty = 0x1;
+		static constexpr unsigned int track_dirty = 0x2;
+		unsigned int m_dirty_bits = handle_dirty|track_dirty;
+
+		unsigned int m_border_thickness = 0;
+		std::shared_ptr<font_handling::font const> m_font;
+
+		float m_margin = 0.0f;
+		rgba_pixel m_bg_tint;
+		rgba_pixel m_fg_tint;
+
+		main::immutable_shared_texture m_null_texture;
+		main::unique_texture m_track;
+		main::unique_texture m_handle;
+
+		main::fb_size m_current_size;
+
+		state m_state_saved = state::released;
+		state m_state_current = state::released;
+		float m_value = 0.0f;
+		enum orientation m_orientation;
+	};
+}
+
+#endif
