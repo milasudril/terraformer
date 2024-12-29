a4e6aef7748db6fa61d00b319c1c3d5588d9e935
Torbjörn Rathsman <milasudril@gmail.com>
2024-12-29 19:05:16 +0100

    Add unit test for log_value_map

---
+++ ui/value_maps/log_value_map.test.cpp
@@ -0,0 +1,20 @@
+//@	{"target":{"name": "log_value_map.test"}}
+
+#include "./log_value_map.hpp"
+
+#include <testfwk/testfwk.hpp>
+
+TESTCASE(terraformer_ui_value_maps_log_value_map_verify)
+{
+	terraformer::ui::value_maps::log_value_map map{1.0f/256.0f, 256.0f, 2.0f};
+	EXPECT_EQ(map.min(), 1.0f/256.0f);
+	EXPECT_EQ(map.max(), 256.0f);
+	EXPECT_EQ(map.from_value(1.0f), 0.5f);
+	EXPECT_EQ(map.to_value(0.5f), 1.0f);
+	EXPECT_EQ(map.from_value(256.0f), 1.0f);
+	EXPECT_EQ(map.to_value(1.0f), 256.0f);
+	EXPECT_EQ(map.from_value(1.0f/256.0f), 0.0f);
+	EXPECT_EQ(map.to_value(0.0f), 1.0f/256.0f);
+	EXPECT_EQ(map.from_value(0.0f), 0.0f);
+	EXPECT_EQ(map.from_value(-1.0f), 0.0f);
+}
