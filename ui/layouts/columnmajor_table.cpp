afbb9e616dbb3a57436f7635ac6229c28191ce41
Torbjörn Rathsman <milasudril@gmail.com>
2024-12-31 20:58:10 +0100

    Introduce column-major table

---
+++ ui/layouts/columnmajor_table.cpp
@@ -0,0 +1,80 @@
+//@	{"target":{"name":"columnmajor_table.o"}}
+
+#include "./columnmajor_table.hpp"
+
+terraformer::scaling
+terraformer::ui::layouts::columnmajor_table::update_widget_locations(
+	main::widget_collection_ref& widgets
+) const
+{
+	auto const widget_geometries = widgets.widget_geometries();
+	auto x_offset = margin_x;
+	auto y_offset = margin_y;
+
+	auto const* const rows = m_rowheights.get();
+	size_t row = 0;
+	auto const rowcount = m_rowcount;
+	auto col = m_colwidths.element_indices().front();
+	auto const colcount = std::size(m_colwidths);
+
+	for(auto k : widgets.element_indices())
+	{
+		widget_geometries[k].where = location{
+			x_offset,
+			-y_offset,
+			0.0f,
+		};
+		widget_geometries[k].origin = location{-1.0f, 1.0f, 0.0f};
+
+		y_offset += rows[row];
+		++row;
+		if(row == rowcount)
+		{
+			x_offset += m_colwidths[col];
+			++col;
+			if(col == colcount)
+			{ break; }
+			row = 0;
+			y_offset = margin_y;
+		}
+	}
+
+	return scaling{
+		m_width,
+		m_height,
+		1.0f,
+	};
+}
+
+void terraformer::ui::layouts::columnmajor_table::minimize_cell_sizes(
+	main::widget_collection_ref const& widgets
+)
+{
+	auto const sizes = widgets.sizes();
+	auto rows = m_rowheights.get();
+	auto const rowcount = m_rowcount;
+	std::fill_n(rows, rowcount, 0.0f);
+	m_colwidths.clear();
+	auto max_width = 0.0f;
+	size_t row = 0;
+	auto const indices = widgets.element_indices();
+	for(auto k : indices)
+	{
+		// NOTE: It is assumed that size is 0 if widget is collapsed
+		rows[row] = std::max(sizes[k][1] + margin_y, rows[row]);
+		max_width = std::max(sizes[k][0] + margin_x, max_width);
+
+		++row;
+		if(row == rowcount)
+		{
+			row = 0;
+			m_colwidths.push_back(max_width);
+			max_width = 0.0f;
+		}
+	}
+	if(row != 0)
+	{ m_colwidths.push_back(max_width); }
+
+	m_width = std::accumulate(std::begin(m_colwidths), std::end(m_colwidths), margin_x);
+	m_height = std::accumulate(rows, rows + rowcount, margin_x);
+}
---
+++ ui/layouts/columnmajor_table.hpp
@@ -0,0 +1,39 @@
+//@	{"dependencies_extra":[{"ref":"./columnmajor_table.o", "rel":"implementation"}]}
+
+#ifndef TERRAFORMER_UI_LAYOUTS_COLUMNMAJOR_TABLE_HPP
+#define TERRAFORMER_UI_LAYOUTS_COLUMNMAJOR_TABLE_HPP
+
+#include "lib/array_classes/single_array.hpp"
+#include "lib/common/spaces.hpp"
+#include "ui/main/widget_collection_ref.hpp"
+
+#include <memory>
+
+namespace terraformer::ui::layouts
+{
+	class columnmajor_table
+	{
+	public:
+		explicit columnmajor_table(size_t rowcount):
+			m_rowcount{rowcount},
+			m_rowheights{std::make_unique<float[]>(rowcount)}
+		{}
+
+		scaling update_widget_locations(main::widget_collection_ref& widgets) const;
+
+		void minimize_cell_sizes(main::widget_collection_ref const& widgets);
+
+		float margin_x;
+
+		float margin_y;
+
+	private:
+		size_t m_rowcount;
+		std::unique_ptr<float[]> m_rowheights;
+		single_array<float> m_colwidths;
+		float m_width;
+		float m_height;
+	};
+}
+
+#endif
