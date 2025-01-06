e9bb401de939f2d981177a6de18478c79c4c2e0d
Torbjörn Rathsman <milasudril@gmail.com>
2025-01-06 13:05:29 +0100

    Offset by 1/2 instead of subtracting from w and h

--- lib/generators/plain_generator/plain.cpp
+++ lib/generators/plain_generator/plain.cpp
@@ -15,11 +15,9 @@ void terraformer::replace_pixels(
 {
 	auto const w = output.width();
 	auto const h = output.height();
-	if(w < 2u || h < 2u)
-	{ throw std::runtime_error{"Domain size must be at least 2 x 2 pixels"}; }
 
-	auto const w_float = static_cast<float>(w - 1);
-	auto const h_float = static_cast<float>(h - 1);
+	auto const w_float = static_cast<float>(w);
+	auto const h_float = static_cast<float>(h);
 
 	cubic_spline_control_point const nw_x{
 		.y = params.nw.elevation,
@@ -48,10 +46,10 @@ void terraformer::replace_pixels(
 
 	for(uint32_t y = 0; y != h; ++y)
 	{
-		auto const eta = static_cast<float>(y)/h_float;
+		auto const eta = (static_cast<float>(y) + 0.5f)/h_float;
 		for(uint32_t x = 0; x != w; ++x)
 		{
-			auto const xi = static_cast<float>(x)/w_float;
+			auto const xi = (static_cast<float>(x) + 0.5f)/w_float;
 
 			auto const x_interp_n = interp(nw_x, ne_x, xi);
 			auto const x_interp_s = interp(sw_x, se_x, xi);
