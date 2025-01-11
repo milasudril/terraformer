b5e29d3bef15dfb2b77b5d5f3f558b920341bdbd
Torbjörn Rathsman <milasudril@gmail.com>
2025-01-11 17:49:56 +0100

    Add elevation to debug output

--- app/cli/palette_test_2.cpp
+++ app/cli/palette_test_2.cpp
@@ -1,6 +1,7 @@
 #include "lib/math_utils/interp.hpp"
 #include "lib/math_utils/boundary_sampling_policies.hpp"
 #include "lib/pixel_store/rgba_pixel.hpp"
+#include "ui/value_maps/asinh_value_map.hpp"
 
 #include <pretty/base.hpp>
 #include <pretty/plot.hpp>
@@ -36,23 +37,24 @@ void make_colored_cell(html_color rgb, size_t index, size_t colspan = 1)
 	);
 }
 
-void dump_color_array(std::span<terraformer::rgba_pixel const> colors)
+void dump_color_array(std::span<std::pair<terraformer::rgba_pixel, float> const> colors)
 {
 	puts("<table>");
-	puts("<tr>");
-	for(size_t k = 0; k != std::size(colors); ++k)
+	for(size_t k = std::size(colors); k != 0; --k)
 	{
-		make_colored_cell(make_html_color(colors[k]), k);
-
+		puts("<tr>");
+		make_colored_cell(make_html_color(colors[k - 1].first), k);
+		printf("<td>%.2g</td>", colors[k - 1].second);
+		puts("</tr>");
 	}
-	puts("</tr>");
 	puts("</table>");
 }
 
 int main()
 {
-	static constexpr std::array<float, 4> x_vals{0.0f, 0.5f, 5.0f/6.0f, 6.0f/6.0f};
+	static constexpr std::array<float, 4> x_vals{0.0f, 0.5f, 11.0f/12.0f, 12.0f/12.0f};
 	static constexpr std::array<terraformer::rgba_pixel, 4> y_vals{
+//		terraformer::rgba_pixel{0.5f, 0.0f, 0.5f, 1.0f},
 		terraformer::rgba_pixel{0.0f, 0.0f, 1.0f, 1.0f},
 		terraformer::rgba_pixel{0.0f, 1.0f, 0.0f, 1.0f},
 		terraformer::rgba_pixel{1.0f, 0.0f, 0.0f, 1.0f},
@@ -66,10 +68,18 @@ int main()
 	};
 
 	terraformer::linear_interpolation_table lut{span};
-	std::array<terraformer::rgba_pixel, 13> interp_vals{};
+
+	terraformer::ui::value_maps::asinh_value_map value_map{
+		266.3185546307779f,
+		0.7086205026374324f*6.0f
+	};
+
+	std::array<std::pair<terraformer::rgba_pixel, float>,25> interp_vals{};
 	for(size_t k = 0; k != std::size(interp_vals); ++k)
 	{
-		interp_vals[k] = lut(static_cast<float>(k)/static_cast<float>(std::size(interp_vals) - 1));
+		auto const xi = static_cast<float>(k)/static_cast<float>(std::size(interp_vals) - 1);
+		interp_vals[k].first = lut(xi);
+		interp_vals[k].second = value_map.to_value(xi);
 	}
 	dump_color_array(interp_vals);
 }
