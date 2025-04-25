61c34587d53945bfd94ad21b72b977f0fa4d7a24
Torbjörn Rathsman <milasudril@gmail.com>
2025-04-25 22:11:30 +0200

    More work on layout interface

--- ui/main/layout.hpp
+++ ui/main/layout.hpp
@@ -9,7 +9,7 @@ namespace terraformer::ui::main
 	 * \param available_width the width available for the layout
 	 * \param available_height the height available for the layout
 	 * \param sizes_out sizes computed by the layout
-	 * 
+	 *
 	 * # Layout run (Fixed widths)
 	 *
 	 * 1. Set cell sizes width zero width
@@ -29,35 +29,37 @@ namespace terraformer::ui::main
 	template<class T>
 	concept layout(
 		T& obj,
-		std::span<scaling const> sizes_in, 
-		float available_width, 
+		std::span<scaling const> sizes_in,
+		float available_width,
 		float available_height.
-		std::span<scaling> sizes_out
+		std::span<scaling> sizes_out,
+		std::span<location> locs_out
 	)
 	{
 		/**
 		 * Sets cell sizes to the absolute value given by the sizes
 		 */
 		{obj.set_cell_sizes_to(sizes_in)} -> std::same_as<void>;
-		
+
 		/**
 		 * Adjusts cell widths given available_width
 		 */
 		{obj.adjust_cell_widths(available_width)} -> std::same_as<void>;
-		
+
 		/**
 		 * Adjusts cell heights given available_height
 		 */
 		{obj.adjust_cell_heights(available_height)} -> std::same_as<void>;
-		
+
 		/**
 		 * Fetches the current cell sizes, and returns the size of the layout
 		 */
 		{obj.get_cell_sizes_into(sizes_out)} -> std::same_as<scaling>;
-		
+
 		/**
+		 * Fetches all cell locations into locs_out
 		 */
-		{obj.update_widget_locations()} -> std::same_as<void>;
+		{obj.get_cell_locations_into(locs_out)} -> std::same_as<void>;
 	}
 }
 
