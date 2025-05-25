5f0a4c945e844eff96abe0c03015ce8f4c5c4c32
Torbjörn Rathsman <milasudril@gmail.com>
2025-05-25 20:56:51 +0200

    Add labels to colorbar

--- ui/widgets/colorbar.hpp
+++ ui/widgets/colorbar.hpp
@@ -4,52 +4,36 @@
 #define TERRAFORMER_UI_WIDGETS_COLORBAR_HPP
 
 #include "./value_map.hpp"
-#include "ui/main/widget.hpp"
-#include "ui/main/graphics_backend_ref.hpp"
+#include "./widget_group.hpp"
+#include "./label.hpp"
+#include "ui/layouts/table.hpp"
 #include "ui/value_maps/affine_value_map.hpp"
 
 namespace terraformer::ui::widgets
 {
-	class colorbar:public main::widget_with_default_actions
+	class colorbar:private widget_group<layouts::table>
 	{
 	public:
-		colorbar()
-		{ update_colorbar(); }
-
-		using widget_with_default_actions::handle_event;
+		using widget_group::handle_event;
+		using widget_group::get_children;
+		using widget_group::get_layout;
+		using widget_group::compute_size;
+		using widget_group::confirm_size;
 
 		template<class ValueMap, class ColorMap>
-		explicit colorbar(ValueMap&& vm, ColorMap&& cm):
+		explicit colorbar(iterator_invalidation_handler_ref iihr, ValueMap&& vm, ColorMap&& cm):
+			widget_group{
+				iihr,
+				1u,
+				layouts::table::cell_order::row_major
+			},
 			m_value_map{std::forward<ValueMap>(vm)},
 			m_color_map{std::forward<ColorMap>(cm)}
 		{
-			update_colorbar();
-			update_frame();
+			init();
 		}
 
-		void update_colorbar();
-
-		void update_frame();
-
-		main::widget_layer_stack prepare_for_presentation(main::graphics_backend_ref backend);
-
-		box_size compute_size(main::widget_width_request)
-		{
-			return box_size{
-				static_cast<float>(m_image.frontend_resource().width()),
-				static_cast<float>(m_image.frontend_resource().height()),
-				0.0f
-			};
-		}
-
-		box_size compute_size(main::widget_height_request)
-		{
-			return box_size{
-				static_cast<float>(m_image.frontend_resource().width()),
-				static_cast<float>(m_image.frontend_resource().height()),
-				0.0f
-			};
-		}
+		main::widget_layer_stack prepare_for_presentation(terraformer::ui::main::graphics_backend_ref);
 
 		void theme_updated(main::config const& cfg, main::widget_instance_info)
 		{
@@ -57,6 +41,23 @@ namespace terraformer::ui::widgets
 			m_fg_tint = cfg.output_area.colors.foreground;
 			m_border_thickness = static_cast<uint32_t>(cfg.output_area.border_thickness);
 			update_frame();
+		}
+
+		void update_colorbar();
+
+		void update_frame();
+
+		void update_labels()
+		{
+			auto const value_map_ptr = m_value_map.get().get_pointer();
+			auto const to_value = m_value_map.get().get_vtable().to_value;
+			for(size_t k = 0; k != std::size(m_labels); ++k)
+			{
+				auto const intensity = 1.0f - static_cast<float>(k)/static_cast<float>(std::size(m_labels) - 1);
+				auto const value = to_value(value_map_ptr, intensity);
+				// TODO: siformat should return u8
+				m_labels[k].text(reinterpret_cast<char8_t const*>(siformat(value, 2).c_str()));
+			}
 		}
 
 	private:
@@ -69,11 +70,28 @@ namespace terraformer::ui::widgets
 			}
 		};
 
+		void init()
+		{
+			update_labels();
+			for(size_t k = 0; k!= std::size(m_labels); ++k)
+			{
+				m_labels[k].set_margin(0.0f);
+				append(std::ref(m_labels[k]), terraformer::ui::main::widget_geometry{});
+				layout.set_record_size(k, layouts::table::cell_size::expand{});
+			}
+			layout.params().no_outer_margin = true;
+			layout.params().margin_x = 0.0f;
+			layout.params().margin_y = 0.0f;
+
+			update_colorbar();
+			update_frame();
+		}
 		main::unique_texture m_image;
 		main::unique_texture m_frame;
 		rgba_pixel m_fg_tint;
 		unsigned int m_border_thickness = 0;
 		main::immutable_shared_texture m_null_texture;
+		std::array<label, 13> m_labels;
 	};
 }
 
