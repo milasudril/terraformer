afca0d8b55e8da23f8888fe1d54a8c72c8125ea9
Torbjörn Rathsman <milasudril@gmail.com>
2025-06-08 13:14:57 +0200

    Use Andika as default UI font

--- ui/theming/theme_loader.hpp
+++ ui/theming/theme_loader.hpp
@@ -19,7 +19,7 @@ namespace terraformer::ui::theming
 		auto black_texture = std::make_shared<main::staged_texture const>(generate_black_texture());
 
 		font_handling::font_mapper fonts;
-		auto const fontfile = fonts.get_path("sans-serif");
+		auto const fontfile = fonts.get_path("Andika, Verdana, Comic Sans MS, sans-serif");
 		auto const body_text = std::make_shared<font_handling::font>(fontfile);
 		body_text->set_font_size(11);
 
