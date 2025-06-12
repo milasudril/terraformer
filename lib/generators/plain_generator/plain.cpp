6ed553ef354e0da49abf2920dad64111a061c9f6
Torbjörn Rathsman <milasudril@gmail.com>
2025-06-12 19:24:15 +0200

    Introduce new names to destinguish from grad-only

--- lib/generators/plain_generator/plain.cpp
+++ lib/generators/plain_generator/plain.cpp
@@ -9,22 +9,19 @@
 
 namespace
 {
-	struct boundary_curve_descriptor
+	struct boundary_curve_interp_descriptor
 	{
 		terraformer::bounded_value<terraformer::open_open_interval{0.0f, 1.0f}, 0.5f> x_m{0.5f};
 
 		float y_0 = 0.0f;
 		float y_m = 0.0f;
 		float y_1 = 0.0f;
-		float ddx_0 = 0.0f;
-		float ddx_m = 0.0f;
-		float ddx_1 = 0.0f;
 	};
 
-	class boundary_curve
+	class boundary_curve_interp
 	{
 	public:
-		constexpr explicit boundary_curve(boundary_curve_descriptor const& bcd):
+		constexpr explicit boundary_curve_interp(boundary_curve_interp_descriptor const& bcd):
 			m_poly{
 				make_polynomial(
 					terraformer::cubic_spline_control_point{
@@ -44,6 +41,55 @@ namespace
 					terraformer::cubic_spline_control_point{
 						.y = bcd.y_1,
 						.ddx = 0.0f
+					}
+				)
+			},
+			m_xm{bcd.x_m}
+		{
+		}
+
+		constexpr float operator()(float x) const
+		{
+			return (x < m_xm)? m_poly[0](x/m_xm) : m_poly[1]((x - m_xm)/(1.0f - m_xm));
+		}
+
+	private:
+		std::array<terraformer::polynomial<3>, 2> m_poly;
+		terraformer::bounded_value<terraformer::open_open_interval{0.0f, 1.0f}, 0.5f> m_xm;
+	};
+
+	struct boundary_curve_grad_descriptor
+	{
+		terraformer::bounded_value<terraformer::open_open_interval{0.0f, 1.0f}, 0.5f> x_m{0.5f};
+
+		float ddx_0 = 0.0f;
+		float ddx_m = 0.0f;
+		float ddx_1 = 0.0f;
+	};
+
+	class boundary_curve_grad
+	{
+	public:
+		constexpr explicit boundary_curve_grad(boundary_curve_grad_descriptor const& bcd):
+			m_poly{
+				make_polynomial(
+					terraformer::cubic_spline_control_point{
+						.y = 0.0f,
+						.ddx = bcd.ddx_0
+					},
+					terraformer::cubic_spline_control_point{
+						.y = 0.0f,
+						.ddx = bcd.ddx_m
+					}
+				),
+				make_polynomial(
+					terraformer::cubic_spline_control_point{
+						.y = 0.0f,
+						.ddx = bcd.ddx_m
+					},
+					terraformer::cubic_spline_control_point{
+						.y = 0.0f,
+						.ddx = bcd.ddx_1
 					}
 				)
 			},
@@ -92,51 +138,39 @@ terraformer::plain terraformer::generate(
 	auto const w_float = static_cast<float>(w);
 	auto const h_float = static_cast<float>(h);
 
-	auto const west_to_east_north = boundary_curve(
-		boundary_curve_descriptor{
+	auto const west_to_east_north_interp = boundary_curve_interp(
+		boundary_curve_interp_descriptor{
 			.x_m = params.edge_midpoints.n,
 			.y_0 = params.boundary.nw.elevation,
 			.y_m = params.boundary.n.elevation,
-			.y_1 = params.boundary.ne.elevation,
-			.ddx_0 = 0.0f,
-			.ddx_m = 0.0f,
-			.ddx_1 = 0.0f
+			.y_1 = params.boundary.ne.elevation
 		}
 	);
 
-	auto const west_to_east_south = boundary_curve(
-		boundary_curve_descriptor{
+	auto const west_to_east_south_interp = boundary_curve_interp(
+		boundary_curve_interp_descriptor{
 			.x_m = params.edge_midpoints.s,
 			.y_0 = params.boundary.sw.elevation,
 			.y_m = params.boundary.s.elevation,
-			.y_1 = params.boundary.se.elevation,
-			.ddx_0 = 0.0f,
-			.ddx_m = 0.0f,
-			.ddx_1 = 0.0f
+			.y_1 = params.boundary.se.elevation
 		}
 	);
 
-	auto const north_to_south_west = boundary_curve(
-		boundary_curve_descriptor{
+	auto const north_to_south_west_interp = boundary_curve_interp(
+		boundary_curve_interp_descriptor{
 			.x_m = params.edge_midpoints.w,
 			.y_0 = params.boundary.nw.elevation,
 			.y_m = params.boundary.w.elevation,
-			.y_1 = params.boundary.sw.elevation,
-			.ddx_0 = 0.0f,
-			.ddx_m = 0.0f,
-			.ddx_1 = 0.0f
+			.y_1 = params.boundary.sw.elevation
 		}
 	);
 
-	auto const north_to_south_east = boundary_curve(
-		boundary_curve_descriptor{
+	auto const north_to_south_east_interp = boundary_curve_interp(
+		boundary_curve_interp_descriptor{
 			.x_m = params.edge_midpoints.e,
 			.y_0 = params.boundary.ne.elevation,
 			.y_m = params.boundary.e.elevation,
-			.y_1 = params.boundary.se.elevation,
-			.ddx_0 = 0.0f,
-			.ddx_m = 0.0f,
-			.ddx_1 = 0.0f
+			.y_1 = params.boundary.se.elevation
 		}
 	);
 
@@ -175,48 +209,36 @@ terraformer::plain terraformer::generate(
 			auto const xi  =  xi_in*cos_theta + eta_in*sin_theta + 0.5f;
 			auto const eta = -xi_in*sin_theta + eta_in*cos_theta + 0.5f;
 
-			auto const north_to_south = boundary_curve(
-				boundary_curve_descriptor{
-				.x_m = lerp(params.edge_midpoints.w, params.edge_midpoints.e, xi),
-				.y_0 = west_to_east_north(xi),
-				.y_m = y_m_ns(xi),
-				.y_1 = west_to_east_south(xi),
-				.ddx_0 = 0.0f,
-				.ddx_m = 0.0f,
-				.ddx_1 = 0.0f
+			auto const north_to_south = boundary_curve_interp(
+				boundary_curve_interp_descriptor{
+					.x_m = lerp(params.edge_midpoints.w, params.edge_midpoints.e, xi),
+					.y_0 = west_to_east_north_interp(xi),
+					.y_m = y_m_ns(xi),
+					.y_1 = west_to_east_south_interp(xi)
 				}
 			);
 
-			auto const north_to_south_grad = boundary_curve(
-				boundary_curve_descriptor{
-				.x_m = lerp(params.edge_midpoints.w, params.edge_midpoints.e, xi),
-				.y_0 = 0.0f,
-				.y_m = 0.0f,
-				.y_1 = 0.0f,
-				.ddx_0 = 0.0f,
-				.ddx_m = 0.0f,
-				.ddx_1 = 0.0f
+			auto const north_to_south_grad = boundary_curve_grad(
+				boundary_curve_grad_descriptor{
+					.x_m = lerp(params.edge_midpoints.w, params.edge_midpoints.e, xi),
+					.ddx_0 = 0.0f,
+					.ddx_m = 0.0f,
+					.ddx_1 = 0.0f
 				}
 			);
 
-			auto const west_to_east = boundary_curve(
-				boundary_curve_descriptor{
+			auto const west_to_east = boundary_curve_interp(
+				boundary_curve_interp_descriptor{
+					.x_m = lerp(params.edge_midpoints.n, params.edge_midpoints.s, eta),
+					.y_0 = north_to_south_west_interp(eta),
+					.y_m = y_m_we(eta),
+					.y_1 = north_to_south_east_interp(eta)
+				}
+			);
+
+			auto const west_to_east_grad = boundary_curve_grad(
+				boundary_curve_grad_descriptor{
 				.x_m = lerp(params.edge_midpoints.n, params.edge_midpoints.s, eta),
-				.y_0 = north_to_south_west(eta),
-				.y_m = y_m_we(eta),
-				.y_1 = north_to_south_east(eta),
-				.ddx_0 = 0.0f,
-				.ddx_m = 0.0f,
-				.ddx_1 = 0.0f
-				}
-			);
-
-			auto const west_to_east_grad = boundary_curve(
-				boundary_curve_descriptor{
-				.x_m = lerp(params.edge_midpoints.n, params.edge_midpoints.s, eta),
-				.y_0 = 0.0f,
-				.y_m = 0.0f,
-				.y_1 = 0.0f,
 				.ddx_0 = 0.0f,
 				.ddx_m = 0.0f,
 				.ddx_1 = 0.0f
