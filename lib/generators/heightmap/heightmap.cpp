8a2856face6f8a29589db4029c776c8ba8cc8a6c
Torbjörn Rathsman <milasudril@gmail.com>
2025-06-15 09:17:43 +0200

    Bugfix

--- lib/generators/heightmap/heightmap.cpp
+++ lib/generators/heightmap/heightmap.cpp
@@ -12,7 +12,7 @@ terraformer::grayscale_image terraformer::generate(heightmap_descriptor const& d
 	auto const plain = generate(descriptor.domain_size, descriptor.generators.plain);
 	auto const rolling_hills = generate(descriptor.domain_size, descriptor.generators.rolling_hills);
 
-	auto const output_width = std::max(plain.width(), rolling_hills.height());
+	auto const output_width = std::max(plain.width(), rolling_hills.width());
 	auto const output_height = std::max(plain.height(), 	rolling_hills.height());;
 
 	grayscale_image ret{output_width, output_height};
