ec812b9faae23f44914b872bba6dba8fba1dbd05
Torbjörn Rathsman <mialsudril@gmail.com>
2025-06-21 17:29:51 +0200

    Introduce atan_value_map for slopes
    Perhaps more intuitive to let the knob indicate the actual angle

---
+++ ui/value_maps/atan_value_map.hpp
@@ -0,0 +1,40 @@
+#ifndef TERRAFORMER_UI_VALUE_MAPS_ATAN_VALUE_MAP_HPP
+#define TERRAFORMER_UI_VALUE_MAPS_ATAN_VALUE_MAP_HPP
+
+#include <cmath>
+#include <limits>
+#include <cstdio>
+
+namespace terraformer::ui::value_maps
+{
+	class atan_value_map
+	{
+	public:
+		constexpr explicit atan_value_map(float max, bool reverse = false) noexcept:
+			m_theta_max{reverse? -std::atan(max): std::atan(max)}
+		{}
+		
+		constexpr float max() const noexcept
+		{ return std::tan(m_theta_max); }
+
+		constexpr float min() const noexcept
+		{ return -std::tan(m_theta_max); }
+
+		constexpr float from_value(float x) const noexcept
+		{ 
+			return (std::atan(x) + m_theta_max)/(2.0f*m_theta_max);
+		}
+
+		constexpr float to_value(float x) const noexcept
+		{
+			printf("to_value (x = %.8g)\n", x);
+			auto const theta = m_theta_max*std::lerp(-1.0f, 1.0f, x);
+			return std::tan(theta); 
+			
+		}
+
+	private:
+		float m_theta_max;
+	};
+}
+#endif
