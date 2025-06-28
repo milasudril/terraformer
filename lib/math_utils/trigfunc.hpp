dc2ebbec0ecd73d9e79336336e836c8e5c240ead
Torbjörn Rathsman <milasudril@gmail.com>
2025-06-28 10:12:34 +0200

    Implement function to compute distance from origin to edge for a rectangle

--- lib/math_utils/trigfunc.hpp
+++ lib/math_utils/trigfunc.hpp
@@ -2,6 +2,7 @@
 #define TERRAFORMER_TRIGFUNC_HPP
 
 #include "./mod.hpp"
+#include "lib/common/spaces.hpp"
 
 #include <numbers>
 
@@ -12,6 +13,13 @@ namespace terraformer
 		auto const xi = 2.0f*(0.5f - mod(x/(2.0f*std::numbers::pi_v<float>), 1.0f));
 		return -(xi*(xi - 1.0f)*(xi + 1.0f)
 			*((13968.0f/31680.0f)*(xi*xi)*(xi*xi) - (63304.0f/31680.0f)*(xi*xi) + 99433.0f/31680.0f));
+	}
+
+	constexpr auto distance_from_origin_to_edge_xy(box_size box, float theta)
+	{
+		auto const factor = abs(scaling{std::cos(theta), std::sin(theta), 1.0f});
+		box /= factor;
+		return 0.5f*std::min(box[0], box[1]);;
 	}
 }
 
--- lib/math_utils/trigfunc.test.cpp
+++ lib/math_utils/trigfunc.test.cpp
@@ -1,6 +1,7 @@
 //@	{"target":{"name":"trigfunc.test"}}
 
 #include "./trigfunc.hpp"
+#include "lib/common/spaces.hpp"
 
 #include <testfwk/testfwk.hpp>
 
@@ -11,5 +12,41 @@ TESTCASE(terraformer_approx_sine)
 	{
 		auto const x = dx*static_cast<float>(k);
 		printf("%.8g %.8g\n", x, terraformer::approx_sine(x));
+	}
+}
+
+
+TESTCASE(terraformer_distance_from_origin_to_edge_xy_box)
+{
+	{
+		terraformer::box_size const b{2.0f, 1.0f, 0.0f};
+		auto constexpr pi = std::numbers::pi_v<float>;
+
+		for(size_t k = 0; k != 4; ++k)
+		{
+			auto const r = terraformer::distance_from_origin_to_edge_xy(b, pi/4.0f + (static_cast<float>(k) - 2.0f)*pi/2.0f);
+			EXPECT_LT(std::abs(r - 1.0f/std::sqrt(2.0f)), 1.0e-8f);
+		}
+
+		EXPECT_EQ(terraformer::distance_from_origin_to_edge_xy(b, 0.0f), 1.0f);
+		EXPECT_EQ(terraformer::distance_from_origin_to_edge_xy(b, pi), 1.0f);
+		EXPECT_EQ(terraformer::distance_from_origin_to_edge_xy(b, -pi/2.0f), 0.5f);
+		EXPECT_EQ(terraformer::distance_from_origin_to_edge_xy(b, pi/2.0f), 0.5f);
+	}
+
+	{
+		terraformer::box_size const b{1.0f, 2.0f, 0.0f};
+		auto constexpr pi = std::numbers::pi_v<float>;
+
+		for(size_t k = 0; k != 4; ++k)
+		{
+			auto const r = terraformer::distance_from_origin_to_edge_xy(b, pi/4.0f + (static_cast<float>(k) - 2.0f)*pi/2.0f);
+			EXPECT_LT(std::abs(r - 1.0f/std::sqrt(2.0f)), 1.0e-8f);
+		}
+
+		EXPECT_EQ(terraformer::distance_from_origin_to_edge_xy(b, 0.0f), 0.5f);
+		EXPECT_EQ(terraformer::distance_from_origin_to_edge_xy(b, pi), 0.5f);
+		EXPECT_EQ(terraformer::distance_from_origin_to_edge_xy(b, -pi/2.0f), 1.0f);
+		EXPECT_EQ(terraformer::distance_from_origin_to_edge_xy(b, pi/2.0f), 1.0f);
 	}
 }
