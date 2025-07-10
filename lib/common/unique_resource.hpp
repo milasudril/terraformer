83f1439b6475a2d622d00a73d827e46fa9b412e7
Torbjörn Rathsman <milasudril@gmail.com>
2025-07-10 08:00:49 +0200

    Implement unique_resource in terms of resource_reference

--- lib/common/unique_resource.hpp
+++ lib/common/unique_resource.hpp
@@ -34,6 +34,12 @@ namespace terraformer
 			m_vtable_pointer{&s_vtable<ReferencedType>}
 		{}
 
+		template<class ReferencedType>
+		explicit resource_reference(ReferencedType* ref):
+			m_handle{ref},
+			m_vtable_pointer{&s_vtable<ReferencedType>}
+		{}
+
 		explicit resource_reference(void* handle, vtable const* vt):
 			m_handle{handle},
 			m_vtable_pointer{vt}
@@ -46,6 +52,9 @@ namespace terraformer
 		{ return m_handle; }
 
 		Vtable const& get_vtable() const
+		{ return *m_vtable_pointer; }
+
+		vtable const& get_full_vtable() const
 		{ return *m_vtable_pointer; }
 
 		operator bool() const
@@ -64,35 +73,30 @@ namespace terraformer
 
 		template<class OwnedType>
 		explicit unique_resource(std::unique_ptr<OwnedType> obj):
-			m_handle{obj.release()},
-			m_vtable_pointer{&resource_reference<Vtable>::template s_vtable<std::remove_cvref_t<OwnedType>>}
+			m_handle{obj.release()}
 		{ }
 
 		template<class OwnedType>
 		requires(!std::is_same_v<std::remove_cvref_t<OwnedType>, unique_resource>)
 		explicit unique_resource(OwnedType&& obj):
-			m_handle{new std::remove_cvref_t<OwnedType>(std::forward<OwnedType>(obj))},
-			m_vtable_pointer{&resource_reference<Vtable>::template s_vtable<std::remove_cvref_t<OwnedType>>}
+			m_handle{new std::remove_cvref_t<OwnedType>(std::forward<OwnedType>(obj))}
 		{ }
 
 		template<class OwnedType, class... Args>
 		explicit unique_resource(std::in_place_type_t<OwnedType>, Args&&... args):
-			m_handle{new OwnedType(std::forward<Args>(args)...)},
-			m_vtable_pointer{&resource_reference<Vtable>::template s_vtable<std::remove_cvref_t<OwnedType>>}
+			m_handle{new OwnedType(std::forward<Args>(args)...)}
 		{}
 
 		~unique_resource()
 		{ reset(); }
 
 		unique_resource(unique_resource&& other):
-			m_handle{std::exchange(other.m_handle, nullptr)},
-			m_vtable_pointer{std::exchange(other.m_vtable_pointer, nullptr)}
+			m_handle{std::exchange(other.m_handle, resource_reference<Vtable>{})}
 		{ }
 
 		unique_resource& operator=(unique_resource&& other)
 		{
 			m_handle = std::exchange(other.m_handle, m_handle);
-			m_vtable_pointer = std::exchange(other.m_vtable_pointer, m_vtable_pointer);
 			other.reset();
 			return *this;
 		}
@@ -102,24 +106,22 @@ namespace terraformer
 		unique_resource& operator=(unique_resource const&) = delete;
 
 		resource_reference<Vtable> get() const
-		{ return resource_reference<Vtable>{m_handle, m_vtable_pointer}; }
+		{ return m_handle; }
 
 		operator bool() const
-		{ return m_handle != nullptr; }
+		{ return static_cast<bool>(m_handle); }
 
 		void reset()
 		{
-			if(m_handle == nullptr)
+			if(!m_handle)
 			{ return; }
 
-			m_vtable_pointer->destroy(m_handle);
-			m_handle = nullptr;
-			m_vtable_pointer = nullptr;
+			m_handle.get_full_vtable().destroy(m_handle.get_pointer());
+			m_handle = resource_reference<Vtable>{};
 		}
 
 	private:
-		void* m_handle{nullptr};
-		typename resource_reference<Vtable>::vtable const* m_vtable_pointer{nullptr};
+		resource_reference<Vtable> m_handle;
 	};
 }
 
