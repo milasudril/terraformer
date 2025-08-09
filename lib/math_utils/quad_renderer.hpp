a955edff82fd14f60f9c48aebbfbbd62008377e3
Torbjörn Rathsman <milasudril@gmail.com>
2025-08-09 16:23:59 +0200

    Work on map_quad_to_unit_square

--- lib/math_utils/quad_renderer.hpp
+++ lib/math_utils/quad_renderer.hpp
@@ -36,6 +36,43 @@ namespace terraformer
 		return q.p1 + displacement{transform*v_impl};
 	}
 
+	inline location map_quad_to_unit_square(quad const& q, location loc)
+	{
+		auto const u = q.p2 - q.p1;
+		auto const v = q.p3 - q.p1;
+		auto const w = location{} + (q.p4 - q.p1) - (u + v);
+
+		auto const w_factors = shuffle(w.get(), 1, 0, 0, 1);
+		auto const uv_factors = shuffle(u.get(), v.get(), 4, 1, 5, 0);
+		auto const prod = w_factors*uv_factors;
+		auto const sum = shuffle(prod, 0, 1, 2, 3) - shuffle(prod, 2, 3, 2, 3);
+
+		auto const det = u[0]*v[1] - u[1]*v[0];
+
+		auto const row_0 = shuffle(v.get(), sum, 0, 1, 2, 4)*geosimd::vec_t{1.0f, -1.0f, 1.0f, 1.0f};
+		auto const row_1 = shuffle(u.get(), sum, 1, 0, 2, 5)*geosimd::vec_t{-1.0f, 1.0f, 1.0f, 1.0f};
+		geosimd::vec_t const row_2{0.0f, 0.0f, 1.0f, 0.0f};
+		geosimd::vec_t const row_3{0.0f, 0.0f, 0.0f, 1.0f};
+
+		std::array const transform{
+			row_0,
+			row_1,
+			row_2,
+			row_3
+		};
+
+		auto const input_vec = (loc - q.p1).get();
+		geosimd::vec_t<float, 4> result{};
+		for(size_t k = 0; k != 4; ++k)
+		{
+			result[k] = inner_product(transform[k], input_vec);
+		}
+
+		result /= det;
+
+		return location{} + displacement{result};
+	}
+
 	template<class PixelType, class Shader>
 	void render_quad(quad const& q, span_2d<PixelType> output, Shader&& shader)
 	{
--- lib/math_utils/quad_renderer.test.cpp
+++ lib/math_utils/quad_renderer.test.cpp
@@ -26,3 +26,26 @@ TESTCASE(terraformer_quad_renderer_map_unit_square_to_quad)
 	auto const loc4 = terraformer::map_unit_square_to_quad(terraformer::location{1.0f, 1.0f, 0.0f}, q);
 	EXPECT_EQ(loc4, q.p4);
 }
+
+TESTCASE(terraformer_quad_renderer_map_quad_to_unit_square)
+{
+	terraformer::quad const q{
+		.p1 = terraformer::location{-1.0f, -4.0f, 0.0f},
+		.p2 = terraformer::location{ 3.0f,  2.0f, 0.0f},
+		.p3 = terraformer::location{-4.0f,  1.0f, 0.0f},
+		.p4 = terraformer::location{-2.0f,  2.0f, 0.0f}
+	};
+
+	auto const loc1 = terraformer::map_quad_to_unit_square(q, q.p1);
+	EXPECT_EQ(loc1, terraformer::location{});
+
+	auto const loc2 = terraformer::map_quad_to_unit_square(q, q.p2);
+	printf("%s\n", to_string(loc2).c_str());
+
+	auto const loc3 = terraformer::map_quad_to_unit_square(q, q.p3);
+	printf("%s\n", to_string(loc3).c_str());
+
+	auto const loc4 = terraformer::map_quad_to_unit_square(q, q.p4);
+	printf("%s\n", to_string(loc4).c_str());
+
+}
