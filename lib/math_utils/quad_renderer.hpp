fbf816f0f8274ed59a91c6f4ab74d7911ce62da5
Torbjörn Rathsman <milasudril@gmail.com>
2025-08-10 09:54:28 +0200

    Compte delta for newton step

--- lib/math_utils/quad_renderer.hpp
+++ lib/math_utils/quad_renderer.hpp
@@ -40,21 +40,62 @@ namespace terraformer
 		return q.p1 + displacement{transform*v_impl};
 	}
 
+	inline auto quad_to_unit_square_compute_delta(quad const& q, displacement current_offset_square, displacement current_offest_quad)
+	{
+		auto const u = q.p2 - q.p1;
+		auto const v = q.p3 - q.p1;
+		auto const w = location{} + (q.p4 - q.p1) - (u + v);
+
+		// x[0]' = u[0] x[0] + v[0] x[1] + w[0] x[0] x[1]  <=>  u[0] x[0] + v[0] x[1] + w[0] x[0] x[1] - x[0]' = 0
+		// x[1]' = u[1] x[0] + v[1] x[1] + w[1] x[0] x[1]  <=>  u[1] x[0] + v[1] x[1] + w[1] x[0] x[1] - x[1]' = 0
+
+		//
+		// J = u[0] + w[0] x[1], v[0] + w[0] x[0]
+		//     u[1] + w[1] x[1], v[1] + w[1] x[0]
+		//
+		auto const x = current_offset_square;
+		auto const y = current_offest_quad;
+
+		auto const denom =  (w[0]*v[1] - v[0]*w[1])*x[1]
+		                   +(u[0]*w[1] - w[0]*u[1])*x[0]
+		                   + u[0]*v[1] - v[0]*u[1];
+		return displacement{
+			 w[0]*x[0]*y[1] + v[0]*y[1] - y[0]*(x[0]*w[1] + v[1]),
+			 x[1]*(y[0]*w[1]     - w[0]*y[1]) - u[0]*y[1] + y[0]*u[1],
+			-y[2]
+		}/denom;
+	}
+
 	inline location map_quad_to_unit_square(quad const& q, location loc)
 	{
 		auto const u = q.p2 - q.p1;
 		auto const v = q.p3 - q.p1;
 		auto const w = location{} + (q.p4 - q.p1) - (u + v);
 
+#if 1
 		auto const w_factors = shuffle(w.get(), 1, 0, 0, 1);
 		auto const uv_factors = shuffle(u.get(), v.get(), 4, 1, 5, 0);
 		auto const prod = w_factors*uv_factors;
 		auto const sum = shuffle(prod, 0, 1, 2, 3) - shuffle(prod, 2, 3, 2, 3);
 
-		auto const det = u[0]*v[1] - u[1]*v[0];
 
-		auto const row_0 = shuffle(v.get(), sum, 0, 1, 2, 4)*geosimd::vec_t{1.0f, -1.0f, 1.0f, 1.0f};
+		auto const row_0 = shuffle(v.get(), sum, 1, 0, 2, 4)*geosimd::vec_t{1.0f, -1.0f, 1.0f, 1.0f};
 		auto const row_1 = shuffle(u.get(), sum, 1, 0, 2, 5)*geosimd::vec_t{-1.0f, 1.0f, 1.0f, 1.0f};
+#else
+		geosimd::vec_t const row_0{
+			v[1],
+			-v[0],
+			0.0f,
+			v[0]*w[1] - v[1]*w[0]
+		};
+
+		geosimd::vec_t const row_1{
+			-u[1],
+			u[0],
+			0.0f,
+			u[1]*w[0] - u[0]*w[1]
+		};
+#endif
 		geosimd::vec_t const row_2{0.0f, 0.0f, 1.0f, 0.0f};
 		geosimd::vec_t const row_3{0.0f, 0.0f, 0.0f, 1.0f};
 
@@ -64,6 +105,7 @@ namespace terraformer
 			row_2,
 			row_3
 		};
+		auto const det = u[0]*v[1] - u[1]*v[0];
 
 		auto const input_vec = (loc - q.p1).get();
 		geosimd::vec_t<float, 4> result{};
@@ -73,6 +115,7 @@ namespace terraformer
 		}
 
 		result /= det;
+		printf("xy = %.8g\n", result[3]);
 
 		return location{} + displacement{result};
 	}
