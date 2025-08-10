04760e423174bc62c4fef2c7cfdd2fc13f5e04a4
Torbjörn Rathsman <milasudril@gmail.com>
2025-08-10 21:40:57 +0200

    More validation

--- lib/math_utils/quad_renderer.test.cpp
+++ lib/math_utils/quad_renderer.test.cpp
@@ -47,6 +47,56 @@ TESTCASE(terraformer_quad_renderer_map_quad_to_unit_square)
 
 	auto const loc4 = terraformer::map_quad_to_unit_square(q, q.p4);
 	EXPECT_LT(distance(loc4, terraformer::location{1.0f, 1.0f, 0.0f}), 1.0e-6f);
+
+	auto const loc_outside1 = terraformer::map_quad_to_unit_square(
+		q,
+		terraformer::location{-3.0f, -2.0f, 0.0f}
+	);
+	EXPECT_EQ(
+			(loc_outside1[0] >= 0.0f && loc_outside1[0] <= 1.0f)
+		&& (loc_outside1[1] >= 0.0f && loc_outside1[1] <= 1.0f),
+		false
+	);
+
+	auto const loc_outside2 = terraformer::map_quad_to_unit_square(
+		q,
+		terraformer::location{2.0f, -2.0f, 0.0f}
+	);
+	EXPECT_EQ(
+			(loc_outside2[0] >= 0.0f && loc_outside2[0] <= 1.0f)
+		&& (loc_outside2[1] >= 0.0f && loc_outside2[1] <= 1.0f),
+		false
+	);
+
+	auto const loc_outside3 = terraformer::map_quad_to_unit_square(
+		q,
+		terraformer::location{0.0f, 3.0f, 0.0f}
+	);
+	EXPECT_EQ(
+			(loc_outside3[0] >= 0.0f && loc_outside3[0] <= 1.0f)
+		&& (loc_outside3[1] >= 0.0f && loc_outside3[1] <= 1.0f),
+		false
+	);
+
+	auto const loc_outside4 = terraformer::map_quad_to_unit_square(
+		q,
+		terraformer::location{-4.0f, 2.0f, 0.0f}
+	);
+	EXPECT_EQ(
+			(loc_outside4[0] >= 0.0f && loc_outside4[0] <= 1.0f)
+		&& (loc_outside4[1] >= 0.0f && loc_outside4[1] <= 1.0f),
+		false
+	);
+
+	auto const loc_inside = terraformer::map_quad_to_unit_square(
+		q,
+		terraformer::location{}
+	);
+	EXPECT_EQ(
+			(loc_inside[0] > 0.0f && loc_inside[0] < 1.0f)
+		&& (loc_inside[1] > 0.0f && loc_inside[1] < 1.0f),
+		true
+	);
 }
 
 TESTCASE(terraformer_quad_renderer_map_quad_to_unit_square_crossed)
@@ -88,4 +138,5 @@ TESTCASE(terraformer_quad_renderer_map_quad_to_unit_square_degenerate)
 
 	// 3 and 4 are indistinguishable
 }
+
 
