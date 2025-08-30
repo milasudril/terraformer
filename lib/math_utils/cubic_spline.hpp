25abd9597d6be493ea2d3166f1dee35aa636f7bc
Torbjörn Rathsman <milasudril@gmail.com>
2025-08-30 14:38:00 +0200

    cubic_spline_control_point is now a template

--- lib/math_utils/cubic_spline.hpp
+++ lib/math_utils/cubic_spline.hpp
@@ -7,29 +7,39 @@
 
 namespace terraformer
 {
+	template<class Point, class Vector>
 	struct cubic_spline_control_point
 	{
-		float y;
-		float ddx;
+		Point y;
+		Vector ddx;
 	};
 
-	constexpr auto make_polynomial(cubic_spline_control_point a, cubic_spline_control_point b)
+	template<class Point, class Vector>
+	constexpr auto make_polynomial(
+		cubic_spline_control_point<Point, Vector> a,
+		cubic_spline_control_point<Point, Vector> b
+	)
 	{
 		auto const y_0 = a.y;
 		auto const y_1 = b.y;
 		auto const A = a.ddx;
 		auto const B = b.ddx;
 
-		// a=B+A-2*y[1]+2*y[0],b=-B-2*A+3*y[1]-3*y[0],c=A,d=y[0]
+		// a=B + A - 2*y[1] + 2*y[0], b = -B -2*A + 3*y[1] - 3*y[0], c=A, d=y[0]
 		auto const cubic = B + A - 2.0f*(y_1 - y_0);
-		auto const quadratic = -B - 2.0f*A + 3.0f*(y_1-y_0);
+		auto const quadratic = -B - 2.0f*A + 3.0f*(y_1 - y_0);
 		auto const linear = A;
-		auto const constant = y_0;
+		auto const constant = y_0 - Point{};
 
 		return polynomial{constant, linear, quadratic, cubic};
 	}
 
-	constexpr auto interp(cubic_spline_control_point a, cubic_spline_control_point b, float x)
+	template<class Point, class Vector>
+	constexpr auto interp(
+		cubic_spline_control_point<Point, Vector> a,
+		cubic_spline_control_point<Point, Vector> b,
+		float x
+	)
 	{	return make_polynomial(a, b)(x); }
 
 	constexpr auto smoothstep(float x)
