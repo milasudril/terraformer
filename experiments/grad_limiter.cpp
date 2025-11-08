244940be3f73b30f05355aecfd833b6985e235c4
Torbjörn Rathsman <milasudril@gmail.com>
2025-11-08 16:07:21 +0100

    Refactor to make it easier to try laplace_limiter

--- experiments/grad_limiter.cpp
+++ experiments/grad_limiter.cpp
@@ -8,6 +8,7 @@
 #include "lib/math_utils/interp.hpp"
 #include <algorithm>
 #include <geosimd/basic_vector.hpp>
+#include <utility>
 
 struct gradient_limiter_descriptor
 {
@@ -71,6 +72,39 @@ float run_pass(
 	return max_grad_out;
 }
 
+template<class PixelMask>
+struct laplace_limiter_descriptor
+{
+	PixelMask mask;
+};
+
+template<class PixelMask>
+void run_pass(
+	terraformer::span_2d<float> output,
+	terraformer::span_2d<float const> input,
+	laplace_limiter_descriptor<PixelMask>&& params
+)
+{
+	for(int32_t y = 0; y != static_cast<int32_t>(output.height()); ++y)
+	{
+		for(int32_t x = 0; x != static_cast<int32_t>(output.width()); ++x)
+		{
+			if(params.mask(static_cast<uint32_t>(x), static_cast<uint32_t>(y)))
+			{
+				using clamp_tag = terraformer::span_2d_extents::clamp_tag;
+				output(x, y) = 0.25f*(
+					 input(x + 1, y, clamp_tag{})
+					+input(x - 1, y, clamp_tag{})
+					+input(x, y + 1, clamp_tag{})
+					+input(x, y + 1, clamp_tag{})
+				);
+			}
+			else
+			{ output(x, y) = input(x, y); }
+		}
+	}
+}
+
 void grad_check(
 	terraformer::span_2d<float> output,
 	terraformer::span_2d<float const> input,
@@ -96,39 +130,94 @@ void grad_check(
 	}
 }
 
-int main()
+void laplace_check(
+	terraformer::span_2d<float> output,
+	terraformer::span_2d<float const> input,
+	float pixel_size
+)
 {
-#if 0
-	terraformer::grayscale_image buffer_a{1024, 1024};
-	buffer_a(512, 512) = 1024.0f;
-#else
-	auto buffer_a = load(
-		terraformer::empty<terraformer::grayscale_image>{},
-		"experiments/rolling_hills.exr"
-	);
-#endif
+	for(int32_t y = 0; y != static_cast<int32_t>(output.height()); ++y)
+	{
+		for(int32_t x = 0; x != static_cast<int32_t>(output.width()); ++x)
+		{
+			using clamp_tag = terraformer::span_2d_extents::clamp_tag;
+			auto const ddx2 = (
+				  input(x + 1, y, clamp_tag{})
+				- 2.0f*input(x, y, clamp_tag{})
+				+ input(x - 1, y, clamp_tag{})
+			)/(pixel_size*pixel_size);
+			auto const ddy2 = (
+				  input(x, y + 1, clamp_tag{})
+				- 2.0f*input(x, y, clamp_tag{})
+				+ input(x, y + 1, clamp_tag{})
+			)/(pixel_size*pixel_size);
+			output(x, y) = ddx2 + ddy2;
+		}
+	}
+}
 
-	auto buffer_b = terraformer::create_with_same_size(buffer_a.pixels());
-	auto pixels_a = buffer_a.pixels();
-	auto pixels_b = buffer_b.pixels();
+terraformer::grayscale_image set_max_gradient_to(
+	terraformer::span_2d<float const> input,
+	gradient_limiter_descriptor grad_limiter
+)
+{
+	auto buffer_in = terraformer::grayscale_image{input};
+	auto buffer_out = terraformer::create_with_same_size<float>(input);
 
-	gradient_limiter_descriptor const grad_limiter{
-		.gradient_threshold = 0.5f,
-		.pixel_size = 8.0f
-	};
-
-	auto ret_prev = run_pass(pixels_b, pixels_a, grad_limiter);
-	std::swap(pixels_a, pixels_b);
+	auto ret_prev = run_pass(buffer_out.pixels(), buffer_in.pixels(), grad_limiter);
+	std::swap(buffer_in, buffer_out);
 	while(true)
 	{
-		auto ret = run_pass(pixels_b, pixels_a, grad_limiter);
+		auto ret =  run_pass(buffer_out.pixels(), buffer_in.pixels(), grad_limiter);;
 		if(std::abs(ret - ret_prev) < 1.0e-4f)
 		{ break; }
 		ret_prev = ret;
-		std::swap(pixels_a, pixels_b);
+		std::swap(buffer_in, buffer_out);
 	}
 
-	store(pixels_b, "/dev/shm/output.exr");
-	grad_check(pixels_a, pixels_b, 8.0f);
-	store(pixels_a, "/dev/shm/grad.exr");
+	return buffer_out;
+}
+
+
+int main()
+{
+	auto after_grad_limiter = set_max_gradient_to(
+		load(
+			terraformer::empty<terraformer::grayscale_image>{},
+			"experiments/rolling_hills.exr"
+		),
+		gradient_limiter_descriptor{
+			.gradient_threshold = 0.5f,
+			.pixel_size = 8.0f
+		}
+	);
+
+	store(after_grad_limiter.pixels(), "/dev/shm/after_grad_limiter.exr");
+#if 0
+		auto buffer_b = terraformer::create_with_same_size(buffer_a.pixels());
+		auto pixels_a = buffer_a.pixels();
+		auto pixels_b = buffer_b.pixels();
+
+
+
+		auto ret_prev = run_pass(pixels_b, pixels_a, grad_limiter);
+		std::swap(pixels_a, pixels_b);
+		while(true)
+		{
+			auto ret = run_pass(pixels_b, pixels_a, grad_limiter);
+			if(std::abs(ret - ret_prev) < 1.0e-4f)
+			{ break; }
+			ret_prev = ret;
+			std::swap(pixels_a, pixels_b);
+		}
+
+		store(pixels_b, "/dev/shm/output.exr");
+		grad_check(pixels_a, pixels_b, 8.0f);
+		store(pixels_a, "/dev/shm/grad.exr");
+
+		auto laplace_buff = terraformer::create_with_same_size(buffer_a.pixels());
+		laplace_check(laplace_buff.pixels(), pixels_b, 8.0f);
+		store(laplace_buff.pixels(), "/dev/shm/laplace.exr");
+	}
+#endif
 }
